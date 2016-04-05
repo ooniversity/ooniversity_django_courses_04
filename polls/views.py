02c38ac262aebfeead67e2f80e13a0898479c746
sashpro <sashpro@mail.ru>
2016-04-06 00:15:52 +0300

    hope, its last conformation1

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,8 @@ from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
 
+
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
