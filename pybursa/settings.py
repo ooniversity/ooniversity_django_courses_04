5dd25c8570c083326f56ed895c1a3b32460f24a7
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-05 17:42:32 +0300

    Added polls dir

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,4 +85,4 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/bootstrap'), )
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), 'templates/admin',)
