38b9f0544062ab844d8080774e7e97b6bba2117b
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-05 17:28:36 +0300

    Added new files

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -82,5 +82,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/bootstrap'), )
 
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import index, contact, student_list, student_detail
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns(
+    '',
     # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
+    url(r'^$', index, name='main'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='student_detail'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
