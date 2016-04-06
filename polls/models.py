223da48256dbfe5c0f36d777f8000ea2d54cbf9b
navka666 <navka6677@gmail.com>
2016-04-06 10:13:32 +0300

    kz3_1 6

--- polls/models.py
+++ polls/models.py
@@ -15,12 +15,14 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    def __unicode__(self):              # __unicode__ on Python 2
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.choice_text
 
 
