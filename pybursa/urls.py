01bdf1d525e03a5dd9d2565fe9ff56d53bfcaad0
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-06 12:58:48 +0300

    Added new dir polls

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import index, contact, student_list, student_detail
-
+from pybursa.views import index, contact, student_list, student_detail
 
 urlpatterns = patterns(
     '',
@@ -10,7 +9,7 @@ urlpatterns = patterns(
     url(r'^contact/$', contact, name='contact'),
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
-    url(r'^polls/', include('polls.urls', namespace='polls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 
     # url(r'^blog/', include('blog.urls')),
     # url(r'^instructors/$', 'instructors.views.instructors_list', name='instructors'),
