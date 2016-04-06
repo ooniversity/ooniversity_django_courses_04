235b8df40490bc2242c6403994effc3aa546b637
sashpro <sashpro@mail.ru>
2016-04-06 14:16:28 +0400

    django PZ 3_2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,10 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from pybursa import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls',namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+
+    #'http://127.0.0.1:8000/'                              =>  index                 => index                      => index.html
+    url(r'^$', 'views.index', name='index'),
+
+    #'http://127.0.0.1:8000/contact/'               => contact               => contact                  => contact.html
+    url(r'^contact/', views.contact, name="contact"),
+ 
+    #'http://127.0.0.1:8000/student_list/'        => student_list       => student_list           => student_list.html
+    url(r'^student_list/', views.student_list, name="student_list"),
+    
+
+    #'http://127.0.0.1:8000/student_detail/'   => student_detail   => student_detail      => student_detail.html
+    url(r'^student_detail/', views.student_detail, name="student_detail"),
+
+
 )
+   
---
+++ pybursa/views.py
@@ -0,0 +1,22 @@
+﻿from django.shortcut import render
+from django.template import RequestContext, loader
+
+def index(request):
+    #template = loader.get_template('index.html')
+    return render(request, 'index.html')
+
+
+
+def contact(request):
+
+    return render(request, 'contact.html')
+
+
+def student_list(request):
+
+    return render(request, 'student_list.html')
+
+
+def student_detail(request):
+
+    return render(request, 'student_detail.html')
