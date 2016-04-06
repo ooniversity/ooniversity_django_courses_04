206c2fcc87f68e18af13043c6f5e537d5de221ee
sashpro <sashpro@mail.ru>
2016-04-06 14:28:14 +0400

    1 try

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,5 +1,8 @@
-﻿from django.shortcut import render
-from django.template import RequestContext, loader
+﻿from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
 
 def index(request):
     #template = loader.get_template('index.html')
