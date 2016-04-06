5e995555ccb66a6a17c6d97d9b5f1a0e2bc06965
zaikays <zaika.y.s@gmail.com>
2016-04-06 17:03:30 +0300

    a bit changed views and urls

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pybursa.views import main_page, contact, studlist,	studdetail
+from pybursa.views import index, contact, student_list,	student_detail
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-   	url(r'^$', main_page, name='main_page'),
+   	url(r'^$', index, name='index'),
 
 	url(r'^contact/', contact, name='contact'),
 
-	url(r'^student_list/', studlist, name='student_list'),
+	url(r'^student_list/', student_list, name='student_list'),
 
-	url(r'^student_detail/', studdetail, name='student_detail'),
+	url(r'^student_detail/', student_detail, name='student_detail'),
    
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
