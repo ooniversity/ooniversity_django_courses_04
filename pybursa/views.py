af48a30ed90ed65d2fbe67eaa973fc47a386519f
VitaliyKhatrus <vitaliy.khatrus@gmail.com>
2016-04-06 17:01:31 +0300

    render indeed render_to_response in pybursa/views.py

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render_to_response
+#from django.shortcuts import render_to_response
+from django.shortcuts import render
 
+
+#def index(request):	
+#	return render_to_response('index.html')
+
+#def contact(request):	
+#	return render_to_response('contact.html')
+
+#def student_list(request):	
+#	return render_to_response('student_list.html')
+
+#def student_detail(request):	
+#	return render_to_response('student_detail.html')
 
 def index(request):	
-	return render_to_response('index.html')
+	return render(request, 'index.html')
 
 def contact(request):	
-	return render_to_response('contact.html')
+	return render(request, 'contact.html')
 
 def student_list(request):	
-	return render_to_response('student_list.html')
+	return render(request, 'student_list.html')
 
 def student_detail(request):	
-	return render_to_response('student_detail.html')
+	return render(request, 'student_detail.html')
