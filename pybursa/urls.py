7f27d2842b6000637a13cbc81f3fab01812d7585
Valery Perepelitsa <perepelitsawrk@gmail.com>
2016-04-06 11:36:15 +0300

    Add templatetag

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import index, contact, student_list, student_detail
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
+    url(r'^$', index),
+    url(r'^contact$', contact),
+    url(r'^student_list$', student_list),
+    url(r'^student_detail$', student_detail),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
