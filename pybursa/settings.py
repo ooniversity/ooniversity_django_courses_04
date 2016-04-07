9610992d2fa9926643fb2ca2ecfde7f38cc7a170
root <root@testdev.richlode.biz>
2016-04-07 21:48:24 +0000

    Bug template dir

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  '../templates', '/polls/templates/polls'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
 ALLOWED_HOSTS = []
 
 
@@ -84,3 +84,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
