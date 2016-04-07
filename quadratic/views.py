1db563708811fe904913215d93ab5d3927b8220f
zaikays <zaika.y.s@gmail.com>
2016-04-08 01:44:56 +0300

    so ugly views but now better

--- quadratic/views.py
+++ quadratic/views.py
@@ -9,28 +9,28 @@ def decision(request):
         a = int(request.GET['a'])
     except ValueError:
         a = request.GET['a']
-        aknd = 'коэффициент не целое число'
-    except MultiValueDictKeyError:
-        a = ''
-        aknd = 'коэффициент не определен'
+        if a == '':
+            aknd = 'коэффициент не определен'
+        else:
+            aknd = 'коэффициент не целое число'
 
     try:
         b = int(request.GET['b'])
     except ValueError:
         b = request.GET['b']
-        bknd = 'коэффициент не целое число'
-    except MultiValueDictKeyError:
-        b = None
-        bknd = 'коэффициент не определен'
+        if b == '':
+            bknd = 'коэффициент не определен'
+        else:
+            bknd = 'коэффициент не целое число'
 
     try:
         c = int(request.GET['c'])
     except ValueError:
         c = request.GET['c']
-        cknd = 'коэффициент не целое число'
-    except MultiValueDictKeyError:
-        c = ''
-        cknd = 'коэффициент не определен'
+        if c == '':
+            cknd = 'коэффициент не определен'
+        else:
+            cknd = 'коэффициент не целое число'
 
     if type(a) == int and type(b) == int and type(c) == int:
         d = b ** 2 - 4 * a * c
@@ -45,6 +45,8 @@ def decision(request):
             x1 = (-b + d ** (1/2.0)) / (2*a)
             x2 = (-b - d ** (1/2.0)) / (2*a)
             xx = 'Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s' % (x1, x2)
-
+    d = 'Дискриминант: %s' %d
+    if a == 0:
+        d = ''
     s = {'a': a, 'b': b, 'c': c, 'd': d, 'xx': xx, 'mk': marker, 'aknd': aknd, 'bknd': bknd, 'cknd': cknd}
     return render(request, 'quadratic/results.html', s)
