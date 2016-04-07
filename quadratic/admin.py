a7fe193467622ef0598203b596cca69d5960d1c8
morozov_n <nickmetal@yandex.ua>
2016-04-07 22:55:22 +0300

    add kz 4

---
+++ quadratic/admin.py
@@ -0,0 +1,4 @@
+# -*- coding: cp1251 -*-
+from django.contrib import admin
+
+# Register your models here.
---
+++ quadratic/models.py
@@ -0,0 +1,4 @@
+# -*- coding: cp1251 -*-
+from django.db import models
+
+# Create your models here.
