1c41942ff8d8773e7ab3858d2c6c21067b15425b
sashpro <sashpro@mail.ru>
2016-04-07 11:08:58 +0300

    square equation

--- quadratic/views.py
+++ quadratic/views.py
@@ -13,33 +13,31 @@ def quadratic_results(request):
         koef[i]=request.GET.__getitem__(koef_names[i]) 
         if  koef[i]=='': 
             dont_solve_flag=True
-            answer+= u'коэффициент не определен\n'
+            answer+= u'коэффициент не определен<br>'
         elif  not koef[i].replace('-','0').isdigit(): 
-            answer+= u'коэффициент не целое число\n'
+            answer+= u'коэффициент не целое число<br>'
             dont_solve_flag=True
         else:
             koef[i]=int(koef[i])
         if koef[i]==0 and i==0:
             dont_solve_flag=True
-            answer+= u'коэффициент при первом слагаемом уравнения не может быть равным нулю\n'
-    if dont_solve_flag:
-        return render(request,'quadratic/results.html',{'responce':answer})    
+            answer+= u'коэффициент при первом слагаемом уравнения не может быть равным нулю<br>'
 
-
-    a,b,c = koef
-    discr1= b*b - 4 * a * c
-    answer+=u"Дескриминант: %d\n" % int(discr1)
-    if discr1 > 0:
-        x1 = (-b + math.sqrt(discr1)) / (2 * a)
-        x2 = (-b - math.sqrt(discr1)) / (2 * a)
-        answer+= u"Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f \n" % (x1,x2)
-    elif discr1 == 0:
-        x = -b / (2 * a)
-        answer += u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f \n' % x
-    else:
-	    #print("Корней нет")
-        answer += u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.\n"
-    return render(request,'quadratic/results.html',{'responce':answer}) 
+    if not dont_solve_flag:
+        a,b,c = koef
+        discr1= b*b - 4 * a * c
+        answer+=u"Дескриминант: %d<br>" % int(discr1)
+        if discr1 > 0:
+            x1 = (-b + math.sqrt(discr1)) / (2 * a)
+            x2 = (-b - math.sqrt(discr1)) / (2 * a)
+            answer+= u"Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f<br>" % (x1,x2)
+        elif discr1 == 0:
+            x1 = -b / (2 * a)
+            answer += u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f<br>' % x1
+        else:
+	        #print("Корней нет")
+            answer += u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.<br>"
+    return render(request,'quadratic/results.html',{'koef':koef, 'discr':discr1, 'x1':x1, 'x2':x2}) 
 
  
 
