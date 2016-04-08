b06fef774635ad57c7954f7b5341b51456e51e90
navka666 <navka6677@gmail.com>
2016-04-08 11:20:12 +0300

    kz4

--- quadratic/views.py
+++ quadratic/views.py
@@ -6,43 +6,52 @@ def quadratic_results(request, a=None, b=None, c=None):
     er_b = ''
     er_c = ''
     er_d = ''
-    q = request.GET
+
+    try:
+        a = int(request.GET['a'])
+    except ValueError:
+        a = request.GET['a']
+        er_a = 'коэффициент не целое число'
+    try:
+        b = int(request.GET['b'])
+    except ValueError:
+        b = request.GET['b']
+        er_b = 'коэффициент не целое число'
+    try:
+        c = int(request.GET['c'])
+    except ValueError:
+        c = request.GET['c']
+        er_c = 'коэффициент не целое число'
+
     d = None
 
-    if q['a'].isdigit():
-        if int(q['a']) == 0:
-            er_a = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-    else:
-        er_a = 'коэффициент не целое число'
-    if not q['a']:
+    if a == 0:
+        er_a = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+
+    if not a and a !=0:
         er_a = 'коэффициент не определен'
 
-    if not q['b'].isdigit():
-        er_b = 'коэффициент не целое число'
-    if not q['b']:
+    if not b:
         er_b = 'коэффициент не определен'
 
-    if not q['c'].isdigit():
-        er_c = 'коэффициент не целое число'
-    if not q['c']:
+    if  not c:
         er_c = 'коэффициент не определен'
 
-    if q['c'].isdigit() and q['b'].isdigit() and q['a'].isdigit() and int(q['a']) != 0:
-        round(d) = discr(int(q['a']), int(q['b']), int(q['c']))
-
+    if isinstance(a, int) and isinstance(b, int) and isinstance(c, int) and a != 0:
+        d = discr(a, b, c)
 
     if d and d < 0:
         er_d = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
     if d and d == 0:
-        x = korni(d, int(q['a']), int(q['b']))
+        x = korni(d, a, b)
         er_d = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s' % x
     if d and d > 0:
-        x = korni(d, int(q['a']), int(q['b']))
+        x = korni(d, a, b)
         er_d = 'Квадратное уравнение имеет два действительных корня: x1 = %d, x2 = %d' % (x[0], x[1])
 
-    return render(request, 'results.html', {'a': q['a'], 'b':q['b'], 'c': q['c'], 'er_a': er_a, 'er_b': er_b, 'er_c': er_c, 'd':d, 'st_d': er_d})
+    return render(request, 'results.html', {'a': a, 'b':b, 'c': c, 'er_a': er_a, 'er_b': er_b, 'er_c': er_c, 'd':d, 'st_d': er_d})
 
-def discr(a,b,c):
+def discr(a, b, c):
     return (b*b-4*a*c)^(1/2)
 
 def korni(d, a, b):
