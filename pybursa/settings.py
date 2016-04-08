a6bb861131f992fad0bcd2a5d852ded325f41b08
SergeiDragunov <fulcrum.ua@gmail.com>
2016-04-09 00:10:57 +0300

    Add urls

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATE_DIRS =(os.path.join(BASE_DIR, 'templates'), )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -82,3 +82,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'), )
