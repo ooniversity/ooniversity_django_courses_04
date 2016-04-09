f6fc25e52ed16e909bdbf15f33de79f16ceeb90d
Dmitry Davydov <dmitry.davydov.total@gmail.com>
2016-04-09 21:31:02 +0300

    django_2_1: done 4 points of tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from polls.models import Choice, Question
 
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -12,9 +12,10 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    inlines = [ChoiceInline]
+
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
