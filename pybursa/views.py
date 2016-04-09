6f798fd9ccb2ad6ef8a039a5f02bf1c2a81ba5bf
Dima Timoshenko <tzd0409@gmail.com>
2016-04-09 18:24:47 +0300

    Attemption #1 KZ_3.2

---
+++ pybursa/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, '../templates/index.html', )
+
+def contact(request):
+    return render(request, '../templates/contact.html', )
+
+def student_list(request):
+    return render(request, '../templates/student_list.html', )
+
+def student_detail(request):
+    return render(request, '../templates/student_detail.html', )            
