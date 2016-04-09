69b4b9061497d21b0e613f450e4eec561385c352
chernish_s <sergey.chernish@gmail.com>
2016-04-09 13:47:14 +0300

    Changed codepage on models.py to utf8

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return u'self.question_text'
+        return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -26,4 +26,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return u'self.choice_text'
+        return self.choice_text
