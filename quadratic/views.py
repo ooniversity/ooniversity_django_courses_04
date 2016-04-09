00bc68faea28cbfdd220062a1c46f26c0516cad6
nikolz_lin_Home <nikolzp@gmail.com>
2016-04-09 23:57:23 +0300

    next-next-next

--- quadratic/views.py
+++ quadratic/views.py
@@ -5,8 +5,7 @@ def quadratic_results(request):
 	'''reshenie kvadratnogo uravneniya'''
 	x,x1,x2 = 0, 0, 0
 	no_koef_a, no_koef_b, no_koef_c= '','',''
-	koef_not_int_a, koef_not_int_b, koef_not_int_c= '','',''
-	if_a_0 = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+	koef_not_int_a, koef_not_int_b, koef_not_int_c, if_a_0 = '','','',''
 	diskr_less_0, dva_kornya, odin_koren, diskrimin = '','','',''
 	
 	try:
@@ -35,10 +34,15 @@ def quadratic_results(request):
 		else:
 			no_koef_c = 'коэффициент не определен'
 		c = (request.GET['c'])
+
 	
+
 	if type(a)==int and type(b)==int and type(c)==int:
 		diskriminant = b**2 - 4*a*c
-		if diskriminant < 0:
+		if a == 0:
+			if_a_0 = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+		
+		elif diskriminant < 0:
 			diskrimin = 'Дискриминант: %d' % diskriminant
 			diskr_less_0 = 'Дискриминант мненьше нуля, квадратное уровнение не имеет действительных корней'
 			
@@ -48,15 +52,11 @@ def quadratic_results(request):
 			odin_koren = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень x1 = x2 = %.1f' %x
 		
 		elif diskriminant > 0:
-			try:
-				x1 = (-b + diskriminant ** (1/2.0))/(2*a)
-				x2 = (-b - diskriminant ** (1/2.0))/(2*a)
-				diskrimin = 'Дискриминант: %d' % diskriminant
-				dva_kornya = 'Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f' % (x1, x2)
-
-			except ZeroDivisionError:
-				return render(request, 'results.html', {'a':a,'b':b, 'c':c, 'if_a_0':if_a_0})
-
+			x1 = (-b + diskriminant ** (1/2.0))/(2*a)
+			x2 = (-b - diskriminant ** (1/2.0))/(2*a)
+			diskrimin = 'Дискриминант: %d' % diskriminant
+			dva_kornya = 'Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f' % (x1, x2)
+			
 	dic = {'a':a, 'b':b, 'c':c, 'x':x, 'x1':x1, 'x2':x2, 'no_koef_a':no_koef_a, 'no_koef_b':no_koef_b, 'no_koef_c':no_koef_c, 'koef_not_int_a':koef_not_int_a, 'koef_not_int_b':koef_not_int_b, 'koef_not_int_c':koef_not_int_c,
-			'diskr_less_0':diskr_less_0, 'diskrimin':diskrimin, 'dva_kornya':dva_kornya, 'odin_koren':odin_koren}
+			'diskr_less_0':diskr_less_0, 'diskrimin':diskrimin, 'dva_kornya':dva_kornya, 'odin_koren':odin_koren, 'if_a_0':if_a_0}
 	return render(request, 'results.html', dic)
