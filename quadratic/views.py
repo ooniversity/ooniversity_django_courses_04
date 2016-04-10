30065d31ee53d4bfddc35d1813ddec4107cc06f4
Max Livshyn <maxi_li@mail.ru>
2016-04-10 19:45:51 +0300

    fixed view and results.html files

--- quadratic/views.py
+++ quadratic/views.py
@@ -2,60 +2,62 @@
 from django.shortcuts import render
 
 def quadratic_results(request):
-	text_a = text_b = text_c = str()
+	param_a = str()
+	param_b = str()
+	param_c = str()
 	disc = {}
-	text_result = {}
+	result = {}
 
 	try:
 		a = int(request.GET['a'])
 	except ValueError:
 		if request.GET['a'].isalpha():
-			text_a = u"коэффициент не целое число"
+			param_a = "коэффициент не целое число"
 			a = request.GET['a']
 		else:
-			text_a = u"коэффициент не определен"
+			param_a = "коэффициент не определен"
 
 	try:
 		b = int(request.GET['b'])
 	except ValueError:
 		if request.GET['b'].isalpha():
-			text_b = u"коэффициент не целое число"
+			param_b = "коэффициент не целое число"
 			b = request.GET['b']
 		else:
-			text_b = u"коэффициент не определен"
+			param_b = "коэффициент не определен"
 
 	try:
 		c = int(request.GET['c'])
 	except ValueError:
 		if request.GET['c'].isalpha():
-			text_c = u"коэффициент не целое число"
+			param_c = "коэффициент не целое число"
 			c = request.GET['c']
 		else:
-			text_c = u"коэффициент не определен"
+			param_c = "коэффициент не определен"
 
 	if 'a' in locals():
 		if a == 0:
-			text_a = u"коэффициент при первом слагаемом уравнения не может быть равным нулю"
+			param_a = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
 
-	if not text_a and not text_b and not text_c :
+	if not param_a and not param_b and not param_c :
 		disc['message'] = "Дискриминант: "
 		disc['value'] = b**2 - 4*a*c
 
 		if disc['value'] < 0:
-			text_result['message'] = u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+			result['message'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
 		elif disc['value'] == 0:
 			x = (-b + disc['value'] ** (1/2.0)) / 2*a
-			text_result['message'] = u"Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = "
-			text_result['value'] = x
+			result['message'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = "
+			result['value'] = x
 		else:
 			x1 = (-b + disc['value'] ** (1/2.0)) / 2*a
 			x2 = (-b - disc['value'] ** (1/2.0)) / 2*a
-			text_result['message'] = u"Квадратное уравнение имеет два действительных корня: " 
-			text_result['value'] = u"x1 = %.1f, x2 = %.1f" % (x1, x2)
+			result['message'] = "Квадратное уравнение имеет два действительных корня: " 
+			result['value'] = "x1 = %.1f, x2 = %.1f" % (x1, x2)
 
 
-	context = {"text_a":text_a, "text_b":text_b, "text_c":text_c, "disc":disc, \
-			"text_result":text_result, 'a':{'message':'a = '}, 'b':{'message':'b = '}, \
+	context = {"param_a":param_a, "param_b":param_b, "param_c":param_c, "disc":disc, \
+			"result":result, 'a':{'message':'a = '}, 'b':{'message':'b = '}, \
 			'c':{'message':'c = '}}
 	
 	if 'a' in locals():
