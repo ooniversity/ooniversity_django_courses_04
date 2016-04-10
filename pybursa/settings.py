f165e7ee60f92b863e84bb9384e190f7c991a60e
Sergej Viziryako <viziryak.sp@gmail.com>
2016-04-10 18:28:47 +0300

    tz_3_2 add

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',    
+    'polls',
+    
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,5 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
