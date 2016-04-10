d9574e7aa99661fa7e5c110b7334c69c5b3af2a7
Bohdan <bohdan.volynskyi@gmail.com>
2016-04-10 17:37:42 +0200

    hope

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^results/', quadratic_results.quadratic_results, name='results'),
+    url(r'^$', quadratic_results.quadratic_results, name='results'),
     url(r'^admin/$', include(admin.site.urls)),
 )
