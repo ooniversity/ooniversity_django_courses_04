61eee310385797fe6666edd78527f67e3c2fe61b
Bohdan <bohdan.volynskyi@gmail.com>
2016-04-10 18:08:40 +0200

    hope#2

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from quadratic import quadratic_results
+from quadratic import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', quadratic_results.quadratic_results, name='results'),
-    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^$', views.quadratic_results, name='results'),
 )
