742b221a056ab29e61fd7740bb5077cce7e4e6fe
Sergej Viziryako <viziryak.sp@gmail.com>
2016-04-10 20:25:02 +0300

    tz_4 add

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^contact/$', contact, name='contact'),
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
-
+    url(r'^quadratic/', include('quadratic.urls')),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
