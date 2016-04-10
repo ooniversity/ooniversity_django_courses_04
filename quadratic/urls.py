94cce24d5d6341049f76cba432831e2bc2afa2c0
Bohdan <bohdan.volynskyi@gmail.com>
2016-04-10 17:14:57 +0200

    Quadratic small changes - renaming view

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from quadratic import views
+from quadratic import quadratic_results
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', views.index, name='index'),
-    url(r'^results/', views.quadratic_results, name='results'),
+    url(r'^results/', quadratic_results.quadratic_results, name='results'),
     url(r'^admin/$', include(admin.site.urls)),
 )
