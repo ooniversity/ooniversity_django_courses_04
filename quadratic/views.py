b71d9be15052de90cd111207ec72bf4ee922b53f
Max Livshyn <maxi_li@mail.ru>
2016-04-10 19:01:04 +0300

    fixed view.py

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,70 +1,64 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+
 def quadratic_results(request):
-	a_param = str()
-	b_param = str()
-	c_param = str()
-	results = {}
-	disc = {}
-	
+    str_a = str_b = str_c = str_d = a = b = c = results = ''
+    try:
+        a = int(request.GET['a'])
+    except ValueError:
+        if not request.GET['a'].isdigit():
+            if request.GET['a'] != '':
+                a = request.GET['a']
+                str_a = 'коэффициент не целое число'
+            else:
+                str_a = 'коэффициент не определен'
 
-	try:
-		a = int(request.GET['a'])
-	except ValueError:
-		if request.GET['a'].isalpha():
-			a_param = u"коэффициент не целое число"
-			a = request.GET['a']
-		else:
-			a_param = u"коэффициент не определен"
+    try:
+        b = int(request.GET['b'])
+    except ValueError:
+        if not request.GET['b'].isdigit():
+            if request.GET['b'] != '':
+                b = request.GET['b']
+                str_b = 'коэффициент не целое число'
+            else:
+                str_b = 'коэффициент не определен'
+    try:
+        c = int(request.GET['c'])
+    except ValueError:
+        if not request.GET['c'].isdigit():
+            if request.GET['c'] != '':
+                c = request.GET['c']
+                str_c = 'коэффициент не целое число'
+            else:
+                str_c = 'коэффициент не определен'
 
-	try:
-		b = int(request.GET['b'])
-	except ValueError:
-		if request.GET['b'].isalpha():
-			b_param = u"коэффициент не целое число"
-			b = request.GET['b']
-		else:
-			b_param = u"коэффициент не определен"
+    if str_a == str_b == str_c == '':
+        if a == 0:
+            str_a = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+        else:
+            d = b ** 2 - 4 * a * c
+            if d > 0:
+                str_d = 'Дискриминант: %d' % d
+                x1 = (-b + d ** (1 / 2.0)) / (2 * a)
+                x2 = (-b - d ** (1 / 2.0)) / (2 * a)
+                results = 'Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f' % (x1, x2)
+            elif d < 0:
+                str_d = 'Дискриминант: %d' % d
+                results = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
+            else:
+                str_d = 'Дискриминант: %d' % d
+                x = -b / 2 * a
+                results = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f' % x
 
-	try:
-		c = int(request.GET['c'])
-	except ValueError:
-		if request.GET['c'].isalpha():
-			c_param = u"коэффициент не целое число"
-			c = request.GET['c']
-		else:
-			c_param = u"коэффициент не определен"
-
-	if 'a' in locals():
-		if a == 0:
-			a_param = u"коэффициент при первом слагаемом уравнения не может быть равным нулю"
-
-	if not a_param and not b_param and not c_param :
-		disc['message'] = "Дискриминант: "
-		disc['value'] = b**2 - 4*a*c
-
-		if disc['value'] < 0:
-			results['message'] = u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-		elif disc['value'] == 0:
-			x = (-b + disc['value'] ** (1/2.0)) / 2*a
-			results['message'] = u"Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = "
-			results['value'] = x
-		else:
-			x1 = (-b + disc['value'] ** (1/2.0)) / 2*a
-			x2 = (-b - disc['value'] ** (1/2.0)) / 2*a
-			results['message'] = u"Квадратное уравнение имеет два действительных корня: " 
-			results['value'] = u"x1 = %.1f, x2 = %.1f" % (x1, x2)
-
-
-	context = {"a_param":a_param, "b_param":b_param, "c_param":c_param, "disc":disc, \
-			"results":results, 'a':{'message':'a = '}, 'b':{'message':'b = '}, \
-			'c':{'message':'c = '}}
-	
-	if 'a' in locals():
-		context['a']['value'] = a
-	if 'b' in locals():
-		context['b']['value'] = b
-	if 'c' in locals():
-		context['c']['value'] = c
-	return render(request,'results.html', context)
+    return render(request, 'quadratic/results.html',
+                  {
+                      'a': a,
+                      'b': b,
+                      'c': c,
+                      'str_a': str_a,
+                      'str_b': str_b,
+                      'str_c': str_c,
+                      'str_d': str_d,
+                      'results': results
+                  })
