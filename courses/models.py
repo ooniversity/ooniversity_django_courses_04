c6840ae19b25c1e25ad5925e889254fbf0670f59
zaikays <zaika.y.s@gmail.com>
2016-04-12 12:28:27 +0300

    last home work

---
+++ courses/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Course(models.Model):
+    'Table wich describe lesson'
+    name = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=250)
+    description = models.TextField()
+    def __unicode__(self):
+        return self.name
+
+class Lesson(models.Model):
+    'Table which describe lesson'
+    subject = models.CharField(max_length=150)
+    description = models.TextField()
+    course = models.ForeignKey(Course)
+    order = models.PositiveIntegerField()
+    def __unicode__(self):
+        return self.subject
