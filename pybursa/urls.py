9b07a7cb01a8c1d2e30b68a6aea811a21ef76226
sashpro <sashpro@mail.ru>
2016-04-12 20:59:44 +0400

    3

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     url(r'^quadratic/', include('quadratic.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
+    url(r'^index/', views.index, name='index'),
+    url(r'^$', views.index),
     url(r'^contact/', views.contact, name="contact"),
  
     #url(r'^student_list/', views.student_list, name="student_list"),
