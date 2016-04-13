a8a998830446c33d4550746bb6d0d6c6d87744a8
navka666 <navka6677@gmail.com>
2016-04-13 10:06:07 +0300

    kz5

--- pybursa/views.py
+++ pybursa/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from courses.models import Course
 
-def index(request):
-    return render(request, 'index.html')
+def course_decr(request):
+    courses = Course.objects.all()
+    return render(request, 'index.html', {'courses': courses})
 
 def contact(request):
     return render(request, 'contact.html')
