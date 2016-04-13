d0d4665683b48ae7b9ec175bb21b1039239317d5
Mobil Work Ubunta <mobil.light@gmail.com>
2016-04-13 15:11:10 +0300

    KZ_5_2 version 1.0

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_filter = ['courses']
+    list_display = ['full_name', 'email', 'skype']
+    fieldsets = [
+            ('Personal info', {'fields':['name', 'surname', 'date_of_birth']}),
+            ('Contact info', {'fields':['email','phone','address','skype']}),
+            (None, {'fields':['courses']})
+    ]
+    filter_horizontal = ['courses']
+
+admin.site.register(Student, StudentAdmin)
