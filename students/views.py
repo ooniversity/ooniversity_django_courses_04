ab1930095e0b07ee5a1d57cea3b9094f6402e6ac
Valery Perepelitsa <perepelitsawrk@gmail.com>
2016-04-13 09:17:02 +0300

    Add dynamic v1.2

--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from students.models import Student
 
-def stud(request):
+def list_view(request):
     if request.GET.get('course_id') == None:
         student = Student.objects.all()
         return render(request, 'students/list.html', {'cur_students': student})
@@ -9,6 +9,6 @@ def stud(request):
         student = Student.objects.filter(courses__id=request.GET.get('course_id'))
         return render(request, 'students/list.html', {'cur_students': student})
 
-def list_view(request, student_id):
+def detail(request, student_id):
         student = Student.objects.get(id=student_id)
         return render(request, 'students/detail.html', {'cur_students': student})
