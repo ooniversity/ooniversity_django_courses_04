1e919048355c0a35d352c36817b72fa15b32fb09
sashpro <sashpro@mail.ru>
2016-04-13 05:22:34 +0300

    5.2 1edition

--- students/admin.py
+++ students/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from students.models import Student
 
-admin.site.register(Student)
+class LessonInline(admin.TabularInline):
+    #model = Lesson
+    #extra=0
+    pass
+
+
+class StudentAdmin(admin.ModelAdmin):
+    def fullname(self,obj):
+        return " ".join((obj.name,obj.surname))
+    fullname.short_description = 'Full name'
+    list_filter = ('courses__name',)
+    list_display=['fullname','email','skype']
+    filter_horizontal=['courses']
+    search_fields=['surname','email']
+    fieldsets=(
+       ('Personal info',{'fields':('name','surname','date_of_birth',)}),
+       ('Contact info',{'fields':('email','phone','address','skype',)}),
+       (None,{'fields':('courses',)}),
+       )
+
+admin.site.register(Student,StudentAdmin)
 
--- students/models.py
+++ students/models.py
@@ -6,15 +6,15 @@ from courses.models import Course
 
 
 class Student(models.Model):
-    name = models.CharField(verbose_name='Имя',max_length=50)
-    surname = models.CharField(verbose_name='Фамилия',max_length=50)
-    date_of_birth = models.DateField(verbose_name='Дата рождения')
-    email = models.EmailField(verbose_name='E-mail')
-    phone = models.CharField(verbose_name='Телефон',max_length=20)
-    address = models.CharField(verbose_name='Адрес', max_length=100)
+    name = models.CharField(verbose_name='Nane',max_length=50)
+    surname = models.CharField(verbose_name='Surname',max_length=50)
+    date_of_birth = models.DateField(verbose_name='Date of birth')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(verbose_name='Phone',max_length=20)
+    address = models.CharField(verbose_name='Address', max_length=100)
     skype = models.CharField(verbose_name='Skype',max_length=50)
-    courses = models.ManyToManyField(Course)
-    
+    courses = models.ManyToManyField(Course,verbose_name='Courses')
+
     def __unicode__(self):
         return  " ".join((self.name, self.surname,)) 
 
