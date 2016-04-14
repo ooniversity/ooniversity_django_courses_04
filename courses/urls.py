f7d1c90a9e115e146b53fd0ad1c25c63073e8beb
morozov_n <nickmetal@yandex.ua>
2016-04-14 11:39:08 +0300

    errors

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import patterns, url
 from courses import views
 
 urlpatterns = patterns('',
-    # url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
-    # url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
-
 )
