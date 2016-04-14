739196acfa3afd770d6be324f08f9bc1c5873a12
Nikolz <nikolzp@gmail.com>
2016-04-14 18:56:54 +0300

    1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quadratic/', include('quadratic.urls')),
     url(r'^courses/', include('courses.urls', namespace='courses')),
+    url(r'^students/', include('students.urls', namespace='students')),
 )
--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from models import Student
 
-# Create your views here.
+
+def list_view(request):
+	deskr = Student.objects.all()
+	mtm = Student.objects.filter(student__courses)
+
+	return render(request, 'students/list.html', {'deskr':deskr, 'mtm':mtm})
