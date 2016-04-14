1bd11478727e231695b2f4226c8420324b4ed675
Dmitry Davydov <dmitry.davydov.total@gmail.com>
2016-04-14 20:58:17 +0300

    django_4_1: in progress

--- pybursa/views.py
+++ pybursa/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    parameters = {
+        'courses': (
+            {'id': 1, 'name': 'Course name 1', 'description': 'Course Description 1'},
+            {'id': 2, 'name': 'Course name 2', 'description': 'Course Description 2'},
+            {'id': 3, 'name': 'Course name 3', 'description': 'Course Description 3'},
+        ),
+    }
+    return render(request, 'index.html', parameters)
 
 
 def contact(request):
