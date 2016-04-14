d9d3324257249747a1f682e77fac2b8daaa7ca9c
Mobil Work Ubunta <mobil.light@gmail.com>
2016-04-14 16:17:55 +0300

    KZ_6 Ver. 1.0

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,4 @@ from courses.models import Course, Lesson
 def detail(request, course_id):
     course_info = get_object_or_404(Course, id=int(course_id))
     lesson_list = Lesson.objects.filter(course_id=course_id)
-    lesson_list.order_by('order')
-    course_get = '?course_id=%d' % course_info.id
-    return render(request, 'courses/detail.html', {'lessons':lesson_list, 'course': course_info, 'course_get': course_get})
+    return render(request, 'courses/detail.html', {'lessons':lesson_list, 'course': course_info})
