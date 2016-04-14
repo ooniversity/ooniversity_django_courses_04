d07645317cc2da68d25e45f4f926e688aa377908
Mobil Work Ubunta <mobil.light@gmail.com>
2016-04-14 16:36:37 +0300

    KZ_6 ver.1.1

--- coaches/admin.py
+++ coaches/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from coaches.models import Coach
 
 class CoachAdmin(admin.ModelAdmin):
-    list_display = ['first_name_field', 'last_name_field', 'phone', 'skype', 'description']
+    list_display = ['first_name_field', 'last_name_field', 'gender', 'skype', 'description']
     list_filter = ['user__is_staff']
 
 admin.site.register(Coach, CoachAdmin)
