5bb05f2335c4dcf0c1c4f1a234a282906a7530ec
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-14 12:54:58 +0300

    Added coaches Ver.1

--- courses/models.py
+++ courses/models.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from coaches.models import Coach
 
 
-class Course (models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=800)
     description = models.TextField()
+    coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
 
     def __unicode__(self):
         return self.name
 
 
-class Lesson (models.Model):
+class Lesson(models.Model):
     subject = models.CharField(max_length=255)
     description = models.TextField()
     course = models.ForeignKey(Course)
