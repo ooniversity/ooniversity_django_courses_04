993ad26392531c548567163bb167ff477c59d098
Vlad Durov <durov.vlad@gmail.com>
2016-04-15 13:44:47 +0300

    code refactoring

--- coaches/views.py
+++ coaches/views.py
@@ -3,12 +3,8 @@ from django.shortcuts import render
 
 from coaches.models import Coach
 from courses.models import Course
-from courses.models import Lesson
 
 
 def detail(request, coach_id):
-    user = get_object_or_404(Coach, pk=coach_id)
-    coach_courses = Course.objects.filter(coach=user).order_by('id')
-    assistan_courses = Course.objects.filter(assistant=user).order_by('id')
-    return render(request, 'coaches/detail.html', {'coach': user,
-                  'coach_courses': coach_courses, 'assistant_courses': assistan_courses})
+    coach = get_object_or_404(Coach, pk=coach_id)
+    return render(request, 'coaches/detail.html', {'coach': coach})
