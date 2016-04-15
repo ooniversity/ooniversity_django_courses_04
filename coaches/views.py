3992cb37164389efb7716bc3b6e9773b493ac0b1
navka666 <navka6677@gmail.com>
2016-04-15 09:04:31 +0300

    kz6_5

--- coaches/views.py
+++ coaches/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from coaches.models import Coach
 
-# Create your views here.
 def detail(request, coach_id):
     coach = Coach.objects.get(id=coach_id)
     coach_course = coach.coach_courses.all()
