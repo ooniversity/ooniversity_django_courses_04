18faded5baf4919686424de81d354f76d8a648a2
Nikolz <nikolzp@gmail.com>
2016-04-15 13:53:30 +0300

    5.1

--- courses/views.py
+++ courses/views.py
@@ -5,9 +5,8 @@ from courses.models import Course, Lesson
 
 
 def detail(request, course_id):
-	descript = Course.objects.get(id=course_id)
-	name = descript.name
+	cours = Course.objects.get(id=course_id)
 	les = Lesson.objects.filter(course_id = course_id)
 	
 
-	return render(request, 'courses/detail.html', {'descript':descript, 'name':name, 'les':les})
+	return render(request, 'courses/detail.html', {'cours':cours,'les':les})
--- students/views.py
+++ students/views.py
@@ -4,11 +4,13 @@ from models import Student
 
 def list_view(request):
 	if request.GET:
-		deskr = Student.objects.filter(courses__id=int(request.GET['course_id']))
+		deskr = Student.objects.filter(courses__id=(request.GET['course_id']))
 	else:
 		deskr = Student.objects.all()
 		
 	return render(request, 'students/list.html', {'deskr':deskr})
 
-def detail(request):
-	return render(request, 'students/detail')
+
+def detail(request, student_id):
+	det = Student.objects.get(id=student_id)
+	return render(request, 'students/detail.html', {'det':det})
