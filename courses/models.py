0d545e6d65df3db04694165a6359a63126cdd77e
Kachur Oleg <kachur.oleg@gmail.com>
2016-04-15 23:43:43 +0300

    KR 5_01 v.02

--- courses/models.py
+++ courses/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length = 150, verbose_name = u'Курс:')
-    short_description = models.CharField(max_length = 200, verbose_name = u'Краткое описание')
-    description = models.TextField(verbose_name = u'Полное описание:')
+    name = models.CharField(max_length = 150)
+    short_description = models.CharField(max_length = 200)
+    description = models.TextField()
     
     def __unicode__(self):
         return self.name
 
 class Lesson(models.Model):
-    subject = models.CharField(max_length = 200, verbose_name = u'Предмет урока:')
-    description = models.TextField(verbose_name = u'Описание урока')
+    subject = models.CharField(max_length = 200)
+    description = models.TextField()
     course = models.ForeignKey(Course)
-    order = models.PositiveIntegerField(verbose_name = u'Lesson order')
+    order = models.PositiveIntegerField()
      
     def __unicode__(self):
         return self.subject
