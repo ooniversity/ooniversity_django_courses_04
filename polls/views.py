1c29eef51418239b02da252a3ebea3c8b2e3c5ca
JackTRussell <JackTRussell@users.noreply.github.com>
2016-04-16 14:45:32 +0300

    Added files via upload

--- polls/views.py
+++ polls/views.py
@@ -1,56 +1,17 @@
-from django.shortcuts import get_object_or_404, render
-<<<<<<< HEAD
-from django.http import HttpResponseRedirect
-=======
-from django.http import HttpResponseRedirect, HttpResponse
->>>>>>> f114d78577c0d563bac959a439787d0446e06ff9
-from django.core.urlresolvers import reverse
-from django.views import generic
+from django.http import HttpResponse
 
-from polls.models import Choice, Question
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
-<<<<<<< HEAD
-=======
-
->>>>>>> f114d78577c0d563bac959a439787d0446e06ff9
 def vote(request, question_id):
-    p = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-<<<<<<< HEAD
-  
-  
-=======
->>>>>>> f114d78577c0d563bac959a439787d0446e06ff9
+    return HttpResponse("You're voting on question %s." % question_id)
--- pybursa/views.py
+++ pybursa/views.py
@@ -1,26 +1,11 @@
-<<<<<<< HEAD
-from django.shortcuts import  render
-
-def index(request):
-	return render(request, 'index.html')
-
-def contact(request):
-	return render(request, 'contact.html')
-
-def student_list(request):
-	return render(request, 'student_list.html')
-
-def student_detail(request):
-	return render(request, 'student_detail.html')
-      
-def results(request):
-	return render(request, 'results.html')      
-=======
 from django.shortcuts import render
 
+from courses.models import Course
+
 
 def index(request):
-    return render(request, 'index.html')
+    courses = Course.objects.only('name', 'short_description', 'id')
+    return render(request, 'index.html', {"courses": courses})
 
 
 def contact(request):
@@ -33,4 +18,3 @@ def student_list(request):
 
 def student_detail(request):
     return render(request, 'student_detail.html')
->>>>>>> f114d78577c0d563bac959a439787d0446e06ff9
