9d988acd1b1ae248962960c794b8f710b5266e1b
Denis <nefaktum@gmail.com>
2016-04-16 19:30:54 +0300

    Files were modified

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from pybursa.views import index, contact, student_list, student_detail
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^$', include('polls.urls', namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index, name='index'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='student_detail')
 )
