52445e681e5f171c2ef8e72f055b72ac6f3fac87
nikolz_lin_Home <nikolzp@gmail.com>
2016-04-17 01:09:49 +0300

    6

---
+++ coaches/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from coaches.models import Coach
+from courses.models import Course
+
+def detail(request, coach_id):
+	coach = Coach.objects.get(id=coach_id)
+	teacher = Course.objects.filter(coach_id=coach_id)
+	assist = Course.objects.filter(assistant_id=coach_id)
+	return render(request, 'coaches/detail.html', {'coach':coach, 'teacher':teacher, 'assist':assist})
+
