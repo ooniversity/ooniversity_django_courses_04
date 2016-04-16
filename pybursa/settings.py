d68e4cbd4059a73befa3350adfe3a05fa9f05632
Max Livshyn <maxi_li@mail.ru>
2016-04-16 14:43:33 +0300

    added new students and course files

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls',
+    'courses',
+    'students'
 )
 
 MIDDLEWARE_CLASSES = (
