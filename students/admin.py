5adce86177752aad1d3e0e4379ffcaec07a34c3f
Arsenii_Petrik <arseniypetrik@gmail.com>
2016-04-16 18:09:41 +0300

    Ended 6.1

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from students.models import Student
+from django.db import models
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ('full_name','email', 'skype',)
+    list_filter = ['courses']
+
+    def full_name(self, Student):
+        return ("%s %s" % (Student.name, Student.surname)).upper()
+    full_name.short_description = 'Full name'
+    fieldsets = [('Personal info', {'fields':['name', 'surname', 'date_of_birth']}),
+                 ('Contact info', {'fields':['email', 'phone', 'address', 'skype']}),
+                 (None, {'fields':['courses']}),]
+    item_filter = ['courses']
+    formfield_overrides = {models.ManyToManyField:{'widget':FilteredSelectMultiple("verbose name", is_stacked=False)}}
+
+
+admin.site.register(Student, StudentAdmin)
