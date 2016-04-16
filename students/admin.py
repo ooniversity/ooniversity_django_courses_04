90625cd13f19ab4aa9c023cd2cfbd60f714df29b
OlhaShyliaieva <olshilyaeva@gmail.com>
2016-04-16 18:06:56 +0300

    Added Admin with horizontal filter

--- students/admin.py
+++ students/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from students.models import Student
 
 #class ArticleAdmin(admin.ModelAdmin):
-   # prepoluted_fields = {'fullname': ('name' , 'surname')}
+   #prepoluted_fields = {'fullname': ('name' , 'surname')}
 
 class StudentAdmin(admin.ModelAdmin):
-    search_fields=['surname', 'email']    
+    search_fields=['surname', 'email']        
     list_display = ['name' , 'surname', 'email', 'skype']
     list_filter = ['courses']
-    fieldsets = [('Personal info', {'fields': ['name','surname', 'date_of_birth']}),('Contact info', {'fields': ['email','phone', 'address', 'skype', 'courses' ]})] 
+    fieldsets = [('Personal info', {'fields': ['name','surname', 'date_of_birth']}),('Contact info', {'fields': ['email','phone', 'address', 'skype']}), ( '',{'fields': ['courses']})]
+    filter_horizontal = ['courses'] 
 
 
     
