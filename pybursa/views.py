c7c0fed7fe33fc41cdc73f937946c9dd142fce4f
Denis <nefaktum@gmail.com>
2016-04-17 20:12:18 +0300

    Files were midified

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from courses.models import Course
 
 # Create your views here.
 #from django.http import Http404, HttpResponseRedirect, HttpResponse
 #from django.core.urlresolvers import reverse
 #from django.views import generic
 
+courses = Course.objects.all()
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'courses': courses})
 
 def contact(request):
     return render(request, 'contact.html')
