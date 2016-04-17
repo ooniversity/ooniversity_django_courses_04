f612d7dc4fde8a55e1cec2437734fb0f512c6f21
morozov_n <nickmetal@yandex.ua>
2016-04-17 13:10:35 +0300

    add reqs in db

--- courses/models.py
+++ courses/models.py
@@ -1,14 +1,19 @@
 # encoding: utf-8
 from django.db import models
-
+from coaches.models import Coach
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=255)
     description = models.TextField()
 
+    coach = models.ForeignKey(Coach,default=None, related_name='coach_courses',null=True, blank=True)
+    assistant = models.ForeignKey(Coach,default=None, related_name='assistant_courses', null=True, blank=True)
+
     def __unicode__(self):
         return self.name
+
+
 
 class Lesson(models.Model):
     subject = models.CharField(max_length=200)
