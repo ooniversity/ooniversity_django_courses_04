48791893dd0bd8b24f69c32e4f85f3b00e9b9f9c
SouthBut <yukhnokostya@gmail.com>
2016-04-17 09:16:32 +0300

    Added files via upload
    add new files

---
+++ coaches/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from coaches.models import Coach
+from courses.models import Course
+
+
+def detail(request, coach_id):
+    coach = Coach.objects.get(id=coach_id)
+    return render(request, 'coaches/detail.html', {'coach': coach})
