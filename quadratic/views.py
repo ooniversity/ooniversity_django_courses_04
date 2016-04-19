0cbd5beda0aae58a6aa56124fbac4cd42bd8a20f
Valery Perepelitsa <perepelitsawrk@gmail.com>
2016-04-19 17:08:24 +0300

    Add equation form v1.0

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,122 +1,67 @@
+# -*- coding: UTF-8 -*-
 from django.shortcuts import render
+from forms import QuadraticForm
 
-class Equation(object):
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+class Coefficient(object):
 
-    def calc_discr(self):
-        self.d = self.b ** 2 - 4 * self.a * self.c
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.value_int = None
+        self.error_message = None
 
-    def get_discr(self):
-        return self.d
+    def is_valid(self):
+        if not self.value:
+            self.error_message = "коэффициент не определен"
+            return False
 
-    def get_eq_root(self, order=1):
-        if order==1:
-            x = (-self.b + self.d ** (1/2.0)) / 2*self.a
-        else:
-            x = (-self.b - self.d ** (1/2.0)) / 2*self.a
-        return x
+        try:
+            self.value_int = int(self.value)
+        except ValueError:
+            self.error_message = "коэффициент не целое число"
+            return False
+
+        if self.name == 'a' and self.value_int == 0:
+            self.error_message = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
+            return False
+        return True
+
+def get_discr(a, b, c):
+    d = b**2 - 4*a*c
+    return d
+
+def get_eq_root(a, b, d, order=1):
+    if order == 1:
+        x = (-b + d**(1/2.0)) / 2*a
+    else:
+        x = (-b - d**(1/2.0)) / 2*a
+    return x
 
 def quadratic_results(request):
-    if str(request.GET.get('a')).isdigit():
-        argument_a = int(request.GET.get('a'))
-    else:
-        try:
-            argument_a = int(request.GET.get('a'))
-        except ValueError:
-            argument_a = str(request.GET.get('a'))
-    if str(request.GET.get('b')).isdigit():
-        argument_b = int(request.GET.get('b'))
-    else:
-        try:
-            argument_b = int(request.GET.get('b'))
-        except ValueError:
-            argument_b = str(request.GET.get('b'))
-    if str(request.GET.get('c')).isdigit():
-        argument_c = int(request.GET.get('c'))
-    else:
-        try:
-            argument_c = int(request.GET.get('c'))
-        except ValueError:
-            argument_c = str(request.GET.get('c'))
-    
-    if type(argument_a) == int and type(argument_b) == int and type(argument_c) == int:
-        qe = Equation(argument_a, argument_b, argument_c)
-        qe.calc_discr()
-        if qe.get_discr() < 0:
-            discremenant = qe.get_discr()
-            return render(request, 'results.html', {'a': argument_a, 
-                                                'b': argument_b, 
-                                                'c': argument_c,
-                                                'discr': discremenant})
+    form = QuadraticForm(request.GET)
+    context = {'error': False}
+    for name_value in ['a', 'b', 'c']:
+        coefficient = Coefficient(name_value, request.GET.get(name_value, ''))
+        if coefficient.is_valid():
+            context[name_value] = coefficient.value_int
         else:
-            discremenant = qe.get_discr()
-            x1 = qe.get_eq_root()
-            x2 = qe.get_eq_root(order=2)
-            if x1 == x2:
-                if type(argument_a) == str:
-                    err_str_a = True
-                else:
-                    err_str_a = False
-                if type(argument_b) == str:
-                    err_str_b = True
-                else:
-                    err_str_b = False
-                if type(argument_c) == str:
-                    err_str_c = True
-                else:
-                    err_str_c = False
-                return render(request, 'results.html', {'a': argument_a, 
-                                                        'b': argument_b, 
-                                                        'c': argument_c,
-                                                        'erra': err_str_a, 
-                                                        'errb': err_str_b, 
-                                                        'errc': err_str_c,
-                                                        'discr': discremenant,
-                                                        'x1': x1})
-            else:
-                if type(argument_a) == str:
-                    err_str_a = True
-                else:
-                    err_str_a = False
-                if type(argument_b) == str:
-                    err_str_b = True
-                else:
-                    err_str_b = False
-                if type(argument_c) == str:
-                    err_str_c = True
-                else:
-                    err_str_c = False
-                discremenant = qe.get_discr()
-                return render(request, 'results.html', {'a': argument_a, 
-                                                        'b': argument_b, 
-                                                        'c': argument_c,
-                                                        'erra': err_str_a, 
-                                                        'errb': err_str_b, 
-                                                        'errc': err_str_c,
-                                                        'discr': discremenant,
-                                                        'x1': x1,
-                                                        'x2': x2})
-    else:
-        if type(argument_a) == str:
-            err_str_a = True
+            context['error'] = True
+            context[name_value + '_error'] = coefficient.error_message
+            context[name_value] = coefficient.value
+    if not context['error']:
+        a = context['a']
+        b = context['b']
+        c = context['c']
+        d = get_discr(a,b,c)
+        if d < 0:
+            result_message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+        elif d == 0:
+            x = get_eq_root(a,b,d)
+            result_message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}.".format(x)
         else:
-            err_str_a = False
-        if type(argument_b) == str:
-            err_str_b = True
-        else:
-            err_str_b = False
-        if type(argument_c) == str:
-            err_str_c = True
-        else:
-            err_str_c = False
-        discremenant = None
-        return render(request, 'results.html', {'a': argument_a, 
-                                                        'b': argument_b, 
-                                                        'c': argument_c,
-                                                        'erra': err_str_a, 
-                                                        'errb': err_str_b, 
-                                                        'errc': err_str_c,
-                                                        'discr': discremenant})
+            x1 = get_eq_root(a,b,d)
+            x2 = get_eq_root(a,b,d,order=2)
+            result_message = "Дискриминант равен нулю, квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}.".format(x1, x2)
+        context.update({'d':d, 'result_message': result_message})
+    context['form'] = form
+    return render(request, 'results.html', context)
