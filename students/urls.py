b7137c801a26095ebb6d1b13d06722f8a34fa71b
sashpro <sashpro@mail.ru>
2016-04-19 11:07:01 +0300

    kz7.3 1ver

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,9 @@ from students import views
 urlpatterns = patterns('',
     url(r'^$', views.list_view,name='list_view'),
     url(r'^(?P<num>\d*)/$', views.detail, name='detail'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^remove/$', views.remove, name='remove'),
+    url(r'^edit/$', views.edit, name='edit'),
 
 
 )
--- students/views.py
+++ students/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from students.models import Student
 from courses.models import Course
+from forms import StudentModelForm
+from django.contrib import messages
+from django.template import RequestContext
 
 def list_view(request):
     course_id=request.GET.get('course_id',None)
@@ -16,3 +19,39 @@ def detail(request,num):
     courses=std.courses.all()#Course.objects.filter(pk__in=std.courses)
     #courses=Course.objects.filter(pk=std__courses__id)
     return render(request,"students/detail.html",{"student":std,"courses":courses})
+
+def create(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            std= form.save()
+            messages.success(request, u'Student %s %s has been successfully added.'%(std.name, std.surname))
+            return redirect('students:list_view')
+                
+    else:
+        form = StudentModelForm()
+    return render(request,"students/add.html",{"form":form})
+
+def edit(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            std= form.save()
+            messages.success(request, u'Student %s %s has been successfully added.'%(std.name, std.surname))
+            return redirect('students:list_view')
+                
+    else:
+        form = StudentModelForm()
+    return render(request,"students/edit.html",{"form":form})
+
+def remove(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            std= form.save()
+            messages.success(request, u'Student %s %s has been successfully added.'%(std.name, std.surname))
+            return redirect('students:list_view')
+                
+    else:
+        form = StudentModelForm()
+    return render(request,"students/remove.html",{"form":form})
