0ebe5d4a3142b07f61faf97c2a7184161880c218
sashpro <sashpro@mail.ru>
2016-04-19 06:10:54 +0300

    kz7.1 1ver

--- coaches/views.py
+++ coaches/views.py
@@ -4,8 +4,8 @@ from courses.models import Course
 
 def detail(request, num):
     coach = Coach.objects.get(pk=int(num))
-    courses_as_assistant = Course.objects.filter(coach__id=int(num))
-    courses_as_coach = Course.objects.filter(assistant__id=int(num))
+    courses_as_coach = Course.objects.filter(coach__id=int(num))
+    courses_as_assistant = Course.objects.filter(assistant__id=int(num))
     #lessons_list=course.lesson.all()
     #lessons_list = Lesson.objects.filter(course_id=int(num))
     return render(request,"coaches/detail.html",{"coach":coach, "as_coach":courses_as_coach, "as_assistant":courses_as_assistant})
