780b1d967997d02e493d5f0955e3c2a978a4fd5e
vetal <svi1976@mail.ru>
2016-04-19 20:09:49 +0300

    KZ7_1 version 1

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from courses.models import Course
+import datetime
 
 def index(request):
 	courses = Course.objects.only('name', 'short_description', 'id')
-	return render(request, 'index.html', {"courses": courses})
+	my_list=['python', 'html', 'javascript']
+	return render(request, 'index.html', {"courses": courses,'my_list':my_list})
 
 def contact(request):
 	return render(request, 'contact.html')
