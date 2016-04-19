330dcedde774cc9be8ab175f691b4d3f84587220
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-19 11:52:29 +0300

    Added new quadratic form Ver.1

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -6,6 +6,12 @@ class QuadraticForm(forms.Form):
     """
     Input variables of the quadratic equation
     """
-    a = forms.FloatField(label='Введите коэффициент a ', widget=forms.TextInput(attrs={'size': '10'}))
-    b = forms.FloatField(label='Введите коэффициент b ', widget=forms.TextInput(attrs={'size': '10'}))
-    c = forms.FloatField(label='Введите коэффициент c ', widget=forms.TextInput(attrs={'size': '10'}))
+    a = forms.FloatField(label="коэффициент a", widget=forms.TextInput(attrs={'size': '10'}))
+    b = forms.FloatField(label="коэффициент b", widget=forms.TextInput(attrs={'size': '10'}))
+    c = forms.FloatField(label="коэффициент c", widget=forms.TextInput(attrs={'size': '10'}))
+
+    def clean_a(self):
+        if int(self.cleaned_data["a"]) == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        else:
+            return self.cleaned_data["a"]
