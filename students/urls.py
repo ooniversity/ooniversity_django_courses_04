58768fd1205edbc0047950b568276ffd33a8534d
sashpro <sashpro@mail.ru>
2016-04-19 11:27:31 +0300

    kz7.3 1ver

--- students/urls.py
+++ students/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', views.list_view,name='list_view'),
     url(r'^(?P<num>\d*)/$', views.detail, name='detail'),
     url(r'^add/$', views.create, name='add'),
-    url(r'^remove/$', views.remove, name='remove'),
-    url(r'^edit/$', views.edit, name='edit'),
+    url(r'^remove/(?P<id>\d+)/$', views.remove, name='remove'),
+    url(r'^edit/(?P<id>\d*)/$', views.edit, name='edit'),
 
 
 )
--- students/views.py
+++ students/views.py
@@ -32,26 +32,23 @@ def create(request):
         form = StudentModelForm()
     return render(request,"students/add.html",{"form":form})
 
-def edit(request):
+def edit(request,id):
+    std_instance=Student.objects.get(pk=id)
     if request.method == 'POST':
-        form = StudentModelForm(request.POST)
+        form = StudentModelForm(request.POST,instance=std_instance)
         if form.is_valid():
             std= form.save()
-            messages.success(request, u'Student %s %s has been successfully added.'%(std.name, std.surname))
+            messages.success(request, u'Info on the student has been sucessfully changed.')
             return redirect('students:list_view')
                 
     else:
-        form = StudentModelForm()
+        form = StudentModelForm(instance=std_instance)
     return render(request,"students/edit.html",{"form":form})
 
-def remove(request):
+def remove(request,id):
+    std=Student.objects.get(pk=id)
     if request.method == 'POST':
-        form = StudentModelForm(request.POST)
-        if form.is_valid():
-            std= form.save()
-            messages.success(request, u'Student %s %s has been successfully added.'%(std.name, std.surname))
-            return redirect('students:list_view')
-                
-    else:
-        form = StudentModelForm()
-    return render(request,"students/remove.html",{"form":form})
+        messages.success(request, u'Info on %s %s has been sucessfully deleted.'%(std.name, std.surname))
+        std.delete()
+        return redirect('students:list_view')
+    return render(request,"students/remove.html",{"student":std})
