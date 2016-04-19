5fddb9a1194657693d8558d6352f53a597a70cd8
Dima Timoshenko <tzd0409@gmail.com>
2016-04-19 17:31:30 +0300

    Переделал все

--- courses/admin.py
+++ courses/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+
 from courses.models import Course, Lesson
 
 class LessonInline(admin.TabularInline):
-    model = Lesson
-    extra = 1
+	model = Lesson
+	extra = 1
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['name', 'short_description', 'coach', 'assistant']
-    search_fields = ['name']
-    inlines = [LessonInline]
-
+	list_display = ['name', 'short_description']
+	search_fields = ['name']
+	inlines = [LessonInline]	
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson)
+
