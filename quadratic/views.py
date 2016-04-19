aa5e2be299079d7dadf3080ed38c3b2216b5d634
Sergey Povazhnuk <sergant_ua@mail.ru>
2016-04-20 02:22:40 +0300

    KZ7_2 ver.02

--- quadratic/views.py
+++ quadratic/views.py
@@ -15,22 +15,23 @@ def quadratic_results(request):
 			b = form.cleaned_data['b']
 			c = form.cleaned_data['c']
 
-		disc['message'] = "Дискриминант: "
-		disc['value'] = b**2 - 4*a*c
+			disc['message'] = "Дискриминант: "
+			disc['value'] = b**2-4*a*c
 
-		if disc['value'] < 0:
-			text_result['message'] = u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-		elif disc['value'] == 0:
-			x = (-b + disc['value'] ** (1/2.0)) / 2*a
-			text_result['message'] = u"Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = "
-			text_result['value'] = x
-		else:
-			x1 = (-b + disc['value'] ** (1/2.0)) / 2*a
-			x2 = (-b - disc['value'] ** (1/2.0)) / 2*a
-			text_result['message'] = u"Квадратное уравнение имеет два действительных корня: " 
-			text_result['value'] = u"x1 = %.1f, x2 = %.1f" % (x1, x2)
+			if disc['value'] < 0:
+				text_result['message'] = u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+			elif disc['value'] == 0:
+				x = (-b + disc['value'] ** (1/2.0)) / 2*a
+				text_result['message'] = u"Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = "
+				text_result['value'] = x
+			else:
+				x1 = (-b + disc['value'] ** (1/2.0)) / 2*a
+				x2 = (-b - disc['value'] ** (1/2.0)) / 2*a
+				text_result['message'] = u"Квадратное уравнение имеет два действительных корня: " 
+				text_result['value'] = u"x1 = %.1f, x2 = %.1f" % (x1, x2)
 	else:
 		form = QuadraticForm()
+	
 	out_result =  context = {"disc":disc, "text_result":text_result, "form":form}
 	
 	return render(request,'quadratic/results.html', out_result)
