03c78f91b5f773ba610cfa3e127e1d48251e425e
sashpro <sashpro@mail.ru>
2016-04-19 10:03:48 +0300

    kz7.2 3ver

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -12,16 +12,8 @@ class QuadraticForm(forms.Form):
         data = self.cleaned_data['a']
         #if  data.replace('.','').replace('-','').isdigit() and 
         if int(data)==0:
-            #self.add_error('a',u'коэффициент при первом слагаемом уравнения не может быть равным нулю')
-            raise forms.ValidationError(u'коэффициент при первом слагаемом уравнения не может быть равным нулю')
+            self.add_error('a',u'коэффициент при первом слагаемом уравнения не может быть равным нулю')
+            #raise forms.ValidationError(u'коэффициент при первом слагаемом уравнения не может быть равным нулю')
         return data
 
-    def clean(self):
-        koef=[0]*3
-        koef_names=('a','b','c')
-        cleaned_data = super(QuadraticForm, self).clean()
-        for i in range(0,3):
-            koef[i]=cleaned_data.get(koef_names[i])        
-            if  not koef[i].replace('.','').replace('-','').isdigit(): 
-                self.add_error(koef_names[i],u'коэффициент не целое число')
 
