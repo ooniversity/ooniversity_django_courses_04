b6ef82a08c893eb0e2c66e2f8b0e68125edd0ed4
navka666 <navka6677@gmail.com>
2016-04-20 16:57:59 +0300

    kz7_3

---
+++ students/forms.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from students.models import Student
+
+class StudentModelForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+
+class StudentModelForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+
+class StudentModelForm(forms.Form):
+
+    pass
