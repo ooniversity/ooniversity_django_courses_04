751e4fe6259db9f8232bc91b06a1d37be2b48cb7
Sergey Povazhnuk <sergant_ua@mail.ru>
2016-04-21 02:33:33 +0300

    KZ7_3 ver.01

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-
 from students import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.list_view, name='list_view'),
 	url(r'^(?P<student_id>\d+)/', views.detail, name='detail'),
+	url(r'^add/$', views.create,  name='student-adding'),
+	url(r'^add/$', views.edit,  name='student-edit'),
+	url(r'^add/$', views.remove,  name='student-remove'),
 )
 
