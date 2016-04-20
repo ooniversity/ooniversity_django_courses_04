cacb10738fc084777aafa477e48dced1cb756905
Vlad Durov <durov.vlad@gmail.com>
2016-04-20 12:44:16 +0300

    sol 7_3_1

--- coaches/views.py
+++ coaches/views.py
@@ -7,7 +7,7 @@ from courses.models import Course
 
 def detail(request, coach_id):
     user = get_object_or_404(Coach, pk=coach_id)
-    coach_courses = Course.objects.filter(coach=user).order_by('id')
-    assistan_courses = Course.objects.filter(assistant=user).order_by('id')
+    coach_courses = Course.objects.filter(coach=user)
+    assistan_courses = Course.objects.filter(assistant=user)
     return render(request, 'coaches/detail.html', {'coach': user,
                   'coach_courses': coach_courses, 'assistant_courses': assistan_courses})
--- students/views.py
+++ students/views.py
@@ -1,9 +1,12 @@
 from django.utils.datastructures import MultiValueDictKeyError
 
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 from courses.models import Course
+from students.forms import StudentModelForm
 from students.models import Student
 
 
@@ -11,18 +14,55 @@ def list_view(request):
     course = None
     try:
         course = get_object_or_404(Course, pk=request.GET['course_id'])
-        students = Student.objects.filter(courses=course).order_by('id')
+        students = Student.objects.filter(courses=course)
     except MultiValueDictKeyError:
-        students = Student.objects.all().order_by('id')
+        students = Student.objects.all()
     context = []
     for student in students:
-        context.append({'student': student,
-                       'student_courses': Course.objects.filter(student=student)})
+        context.append({'student': student})
     return render(request, 'students/list.html',
             {'context': context, 'course':course})
 
 
 def detail(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
-    return render(request, 'students/detail.html', {'student': student,
-                  'student_courses': Course.objects.filter(student=student)})
+    return render(request, 'students/detail.html', {'student': student})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.set_level(request, messages.SUCCESS)
+            messages.success(request, 'Student {0} {1} has been successfully added.'.format(form.cleaned_data.get('name'), form.cleaned_data.get('surname')))
+            return redirect('students:list_view')
+    else:
+        form = StudentModelForm()
+    return render(request, 'students/add.html', {'form': form})
+
+
+def edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.set_level(request, messages.SUCCESS)
+            messages.success(request, 'Info on the student has been sucessfully changed.')
+    else:
+        form = StudentModelForm(instance=student)
+    return render(request, 'students/edit.html', {'form': form})
+
+
+def remove(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        messages.set_level(request, messages.SUCCESS)
+        messages.success(request, 'Info on {0} {1} has been sucessfully deleted.'.format(student.name, student.surname))
+        return redirect('students:list_view')
+    else:
+        messages.set_level(request, messages.WARNING)
+        messages.warning(request, 'Are you sure you want to delete {0} {1}'.format(student.name, student.surname))
+    return render(request, 'students/remove.html')
