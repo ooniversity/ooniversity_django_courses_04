f6dcfabcefced445aecceafc4794ffa602da62d9
VitaliyKhatrus <vitaliy.khatrus@gmail.com>
2016-04-20 19:42:46 +0300

    added modified quadratic 004

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField(label=u"коэффициент a")
-    b = forms.IntegerField(label=u"коэффициент b")
-    c = forms.IntegerField(label=u"коэффициент c")
+    a = forms.IntegerField(label = u'коэффициент a')
+    b = forms.IntegerField(label = u'коэффициент b')
+    c = forms.IntegerField(label = u'коэффициент c')
 
     def clean_a(self):
         data = self.cleaned_data['a']
