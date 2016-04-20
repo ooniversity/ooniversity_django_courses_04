b66d5322318ffa1d006ad10e9bfb2b514f3499e4
zaikays <zaika.y.s@gmail.com>
2016-04-20 14:50:44 +0300

    Third step done

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
 
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'),
-    url(r'^add/$', views.add_student, name='add'),
-    url(r'^edit/(?P<pk>\d+)/$', views.edit_student, name='edit'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
 )
--- students/views.py
+++ students/views.py
@@ -20,7 +20,7 @@ def detail(request, student_id):
     return render(request, 'students/detail.html', {'student': stud_table})
 
 
-def add_student(request):
+def create(request):
     if request.method == 'POST':
         add_form = StudentModelForm(request.POST)
         if add_form.is_valid():
@@ -35,7 +35,7 @@ def add_student(request):
     return render(request, 'students/add.html', {'add_s': add_form})
 
 
-def edit_student(request, pk):
+def edit(request, pk):
     edit_app = Student.objects.get(id=pk)
     if request.method == 'POST':
         edit_form = StudentModelForm(request.POST, instance=edit_app)
@@ -46,5 +46,19 @@ def edit_student(request, pk):
     else:
         edit_form = StudentModelForm(instance=edit_app)
     return render(request, 'students/edit.html', {'edit_form': edit_form})
+
+
+def remove(request, pk):
+    edit_app = Student.objects.get(id=pk)
+    context = {}
+    context['name'] = edit_app.name
+    context['surname'] = edit_app.surname
+    if request.method == 'POST':
+        edit_app.delete()
+        msg = 'Студент {0} {1} был(а) удален(а).'.format(context['name'], context['surname'])
+        messages.success(request, msg)
+
+        return redirect('students:list_view')
+    return render(request, 'students/remove.html', context)
 
 
