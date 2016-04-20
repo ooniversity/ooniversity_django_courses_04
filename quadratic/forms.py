675954917ec9ae04bf3a1061c704ef5298728f2c
navka666 <navka6677@gmail.com>
2016-04-20 09:54:58 +0300

    kz7_2_2

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -3,13 +3,15 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-
-    a = forms.FloatField(label='коэффициент a:')
-    b = forms.FloatField(label='коэффициент b:')
-    c = forms.FloatField(label='коэффициент c:')
+    """
+    Input variables of the quadratic equation
+    """
+    a = forms.IntegerField(label="коэффициент a", widget=forms.TextInput)
+    b = forms.IntegerField(label="коэффициент b", widget=forms.TextInput)
+    c = forms.IntegerField(label="коэффициент c", widget=forms.TextInput)
 
     def clean_a(self):
-        data = self.cleaned_data['a']
-        if data == 0:
-            raise forms.ValidationError(u"коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        return data
+        if self.cleaned_data['a'] == 0:
+            raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю')
+        else:
+            return self.cleaned_data['a']
