158a857a1a4069959e4f8f6ee511006e08eb5365
nikolz_lin_Home <nikolzp@gmail.com>
2016-04-21 07:42:56 +0300

    7.3

--- students/urls.py
+++ students/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<student_id>\d+)/', views.detail, name='detail'),
 	url(r'^add/$', views.create, name='create'),
 	url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
+	url(r'^remove/(?P<student_id>\d+)/$', views.remove, name='remove'),
 
 )
--- students/views.py
+++ students/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from students.models import Student
 from forms import StudentModelForm
@@ -18,16 +19,32 @@ def detail(request, student_id):
 
 def create(request):
 	if request.method == 'POST':
-		model = StudentModelForm(request.POST)
-		if model.is_valid():
-			form = model.save()
-			messages.success(request, 'Student')
+		form = StudentModelForm(request.POST)
+		if form.is_valid():
+			create_st = form.save()
+			messages.success(request, "Student %s %s has been successfully added." % (create_st.name, create_st.surname))
 			return redirect('students:list_view')
 	else:
-		model = StudentModelForm()
-	return render(request, 'students/add.html', {'model':model})
+		form = StudentModelForm()
+	return render(request, 'students/add.html', {'form':form})
 
 def edit(request, student_id):
-	form = Student.objects.get(id=student_id)
-	model = StudentModelForm(instance=form)
+	edit_st = Student.objects.get(id=student_id)
+	if request.method == 'POST':
+		form = StudentModelForm(request.POST, instance=edit_st)
+		if form.is_valid:
+			form.save()
+			messages.success(request, 'Данные изменены')
+	else:
+		form = StudentModelForm(instance=edit_st)
 	return render(request, 'students/edit.html', {'form':form})
+
+
+def remove(request, student_id):
+	remov = Student.objects.get(id=student_id)
+	if request.method == 'POST':
+		remov.delete()
+		messages.success(request, "Info on %s %s has been successfully deleted." % (remov.name, remov.surname))
+		return redirect('students:list_view')
+
+	return render(request, 'students/remove.html', {'remov':remov})
