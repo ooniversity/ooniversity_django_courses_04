fa91177c4579e68cb2d5872089bdf5da4f2d4b7f
root <root@testdev.richlode.biz>
2016-04-21 11:02:00 +0000

    New Structure Template

--- coaches/admin.py
+++ coaches/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from coaches.models import Coach
-#from django.contrib.admin import BooleanFieldListFilter
+
 
 class CoachAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'gender', 'skype', 'description']
