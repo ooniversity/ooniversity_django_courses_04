0bfff8addf46b6ec38f664b6da474083a11c60e8
Sergey Povazhnuk <sergant_ua@mail.ru>
2016-04-21 03:58:04 +0300

    KZ7_3 ver.03

--- students/urls.py
+++ students/urls.py
@@ -4,9 +4,8 @@ from students import views
 urlpatterns = patterns('',
 	url(r'^$', views.list_view, name='list_view'),
 	url(r'^(?P<student_id>\d+)/', views.detail, name='detail'),
-	url(r'^add/$', views.create,  name='student-adding'),
-	url(r'^edit/(?P<pk>>\d+)/', views.edit,  name='edit'),
-	url(r'^remove/(?P<pk>>\d+)/', views.remove,  name='remove'),
-
+    url(r'^add/', views.create, name='add'),
+    url(r'^edit/(?P<id>\d*)/', views.edit, name='edit'),
+    url(r'^remove/(?P<id>\d*)/', views.remove, name='remove')
 )
 
