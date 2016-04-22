3d1ca6266f059f8f6d62518bb0a61090d2f70737
nikolz_lin_Home <nikolzp@gmail.com>
2016-04-22 06:08:26 +0300

    8

--- courses/forms.py
+++ courses/forms.py
@@ -4,7 +4,9 @@ from django import forms
 class CourseModelForm(forms.ModelForm):
 	class Meta:
 		model = Course
+		fields = '__all__'
 
 class LessonModelForm(forms.ModelForm):
 	class Meta:
 		model = Lesson
+		fields = '__all__'
