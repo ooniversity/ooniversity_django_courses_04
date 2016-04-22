fe53f1040e692e1dd34c1a1f82b7f4b96782734b
Arsenii_Petrik <arseniypetrik@gmail.com>
2016-04-22 13:10:56 +0300

    completed 8.1 task

---
+++ courses/forms.py
@@ -0,0 +1,10 @@
+from courses.models import Course, Lesson
+from django import forms
+
+class CourseModelForm(forms.ModelForm):
+    class Meta:
+        model = Course
+
+class LessonModelForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
