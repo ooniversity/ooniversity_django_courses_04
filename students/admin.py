86f2055b8d59d2a3fd5df138e083df2538ced5ea
Dima Timoshenko <tzd0409@gmail.com>
2016-04-23 14:26:43 +0300

    cheat

--- students/admin.py
+++ students/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
-	search_fields = ['surname', 'email']
-	list_display = ['full_name', 'email', 'skype', ]
-
-	def full_name(request, Student):
-	    return "%s %s" % (Student.name, Student.surname)	
-
-	list_filter = ['courses']
-	
+    search_fields = ['surname', 'email']
+    list_filter = ['courses']
+    list_display = ['full_name', 'email', 'skype']
+    fieldsets = [
+            ('Personal info', {'fields':['name', 'surname', 'date_of_birth']}),
+            ('Contact info', {'fields':['email','phone','address','skype']}),
+            (None, {'fields':['courses']})
+    ]
+    filter_horizontal = ['courses']
 
 admin.site.register(Student, StudentAdmin)
-
