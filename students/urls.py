0876427b92174456733930a5a9f7d9ba8a051a40
chernish_s <sergey.chernish@gmail.com>
2016-04-23 18:14:00 +0300

    task7.3ver1.

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,10 @@ from students import views
 
 
 urlpatterns = patterns('',
-                       url(r'^(\d+)/$', views.student_detail_view, name="detail"),
+                       url(r'^(\d+)/$',
+                           views.student_detail_view, name="detail"),
                        url(r'^$', views.student_list_view, name="list_view"),
+                       url(r'^add/$', views.create, name='add'),
+                       url(r'^remove/(\d+)/$', views.remove, name='remove'),
+                       url(r'^edit/(\d+)/$', views.edit, name='edit'),
                        )
