853723a1c5eca767b4d91b4d0acc4f390bc4353c
Bohdan <bohdan.volynskyi@gmail.com>
2016-04-23 12:00:52 +0200

    quadratic updates v4

--- quadratic/views.py
+++ quadratic/views.py
@@ -15,12 +15,15 @@ def quadratic_results(request):
             c = form.cleaned_data['c']
             d = int(b ** 2 - 4 * a * c)
             if d > 0:
-                text = "Квадратное уравнение имеет два действительных корня: x1 = %0.1f, x2 = %0.1f" % (get_results(a, b, d), get_results(a, b, d, 2))
+                x1 = (-b + d**(1/2.0)) / 2*a
+                x2 = (-b - d**(1/2.0)) / 2*a
+                text = "Квадратное уравнение имеет два действительных корня: x1 = %0.1f, x2 = %0.1f" % (x1, x2)
             elif d == 0:
-                text = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (get_results(a, b, d))
+                x = (-b + d**(1/2.0)) / 2*a
+                text = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (x)
             else:
                 text = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-            context['d'] = str(d)
+            context['d'] = d
             context['results'] = text
     else:
         form = QuadraticForm()
@@ -29,10 +32,11 @@ def quadratic_results(request):
     context['form'] = form
     return render(request, 'quadratic/results.html', context)
 
-
+'''
 def get_results(a, b, d, order=1):
     if order == 1:
         x = (-b + d**(1/2.0)) / 2*a
     else:
         x = (-b - d**(1/2.0)) / 2*a
     return x
+'''
