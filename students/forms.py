430380866f5c03f49a9dde793c92711526fc4133
Max Livshyn <maxi_li@mail.ru>
2016-04-23 20:03:33 +0300

    added students forms

---
+++ students/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from students.models import Student
+
+class StudentModelForm(forms.ModelForm):
+    class Meta:
+        model = Student
--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,7 @@ from students import views
 urlpatterns = patterns('',
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^remove/(?P<student_id>\d+)/$', views.remove, name='remove'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
 )
