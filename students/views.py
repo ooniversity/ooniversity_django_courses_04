c627162ad44513169b10baa8089b3ab2f01e5fc4
Dmitry Davydov <dmitry.davydov.total@gmail.com>
2016-04-23 22:49:22 +0300

    django_7_3: implemented features for new/edit/remove students

--- students/views.py
+++ students/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+# -*- coding: UTF-8 -*-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from students.models import Student
+from forms import StudentModelForm
 
 
 def list_view(request):
@@ -33,3 +36,38 @@ def detail(request, student_id):
         'courses': student_courses,
     }
     return render(request, 'students/detail.html', parameters)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            data = form.save()
+            message = u"Student %s %s has been successfully added." % (data.name, data.surname)
+            messages.success(request, message)
+            return redirect('students:list_view')
+    else:
+        form = StudentModelForm()
+    return render(request, 'students/add.html', {'form': form})
+
+
+def edit(request, student_id):
+    student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, u"Info on the student has been sucessfully changed.")
+    else:
+        form = StudentModelForm(instance=student)
+    return render(request, "students/edit.html", {'form': form})
+
+
+def remove(request, student_id):
+    student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        student.delete()
+        message = u"Info on %s %s has been sucessfully deleted." % (student.name, student.surname)
+        messages.success(request, message)
+        return redirect("students:list_view")
+    return render(request, "students/remove.html", {'name': student.name, 'surname': student.surname})
