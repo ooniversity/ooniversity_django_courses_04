36ed120f753f3440b058b2493c2c10b8b041a7e1
Bohdan <bohdan.volynskyi@gmail.com>
2016-04-23 19:49:34 +0200

    add/edit/remove courses and lessons v2

--- courses/views.py
+++ courses/views.py
@@ -60,4 +60,4 @@ def add_lesson(request, course_id):
     else:
         form = LessonModelForm(initial={'course': course_id})
 
-    return render(request, 'courses/add_lesson.html', {"form": form})
+    return render(request, 'courses/add-lesson.html', {"form": form})
