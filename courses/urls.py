2b7e05fd7aa4d0ecc16358a0e1bb67d1e66d98c0
Dmitry Davydov <dmitry.davydov.total@gmail.com>
2016-04-24 20:55:37 +0300

    django_8_1: implemented features for add/edit/remove courses; and add new lessons.

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,8 @@ from courses import views
 urlpatterns = patterns(
     '',
     url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name="add"),
+    url(r'^edit/(\d+)/$', views.edit, name="edit"),
+    url(r'^remove/(\d+)/$', views.remove, name="remove"),
+    url(r'^(\d+)/add_lesson$', views.add_lesson, name="add_lesson"),
 )
