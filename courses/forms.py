fb3cc2daffbf409a16bc7e2068ca4c81264f8b3e
Sergej Viziryako <viziryak.sp@gmail.com>
2016-04-24 16:38:12 +0300

    tz 7, 8

--- courses/forms.py
+++ courses/forms.py
@@ -4,12 +4,10 @@ from courses.models import Course, Lesson
 
 
 class CourseModelForm(forms.ModelForm):
-    """ Создание формы на основании модели курса """
     class Meta:
         model = Course
 
 
 class LessonModelForm(forms.ModelForm):
-    """ Создание формы на основании модели Lesson """
     class Meta:
         model = Lesson
