a8a9133f212011029af24b5d7f3b68129277bb20
OlhaShyliaieva <olshilyaeva@gmail.com>
2016-04-24 17:04:08 +0300

    PyBursa Courses with forms

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,8 @@ from courses import views
 
 urlpatterns = patterns('',
     url(r'^(?P<courses__id>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<courses__id>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<courses__id>\d+)/$', views.remove, name='remove'), 
+    url(r'^(?P<courses__id>\d+)/add_lesson/$', views.add_lesson, name='add-lesson'),
 )
