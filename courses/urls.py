4b2e309a9182e69fe362a1bd6f6b3db13df247fb
zaikays <zaika.y.s@gmail.com>
2016-04-26 21:50:12 +0300

    work 9.2 v1

--- courses/urls.py
+++ courses/urls.py
@@ -3,9 +3,9 @@ from courses import views
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
-    url(r'^add/$', views.add, name='add'),
-    url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
+    url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
     url(r'^(?P<pk>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
 )
