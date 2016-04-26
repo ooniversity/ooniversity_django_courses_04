e940face07b8f30f95ec6536791740cc24a42dc3
zaikays <zaika.y.s@gmail.com>
2016-04-26 22:41:52 +0300

    work 9.2 v2

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
     url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
     url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name='edit'),
-    url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
+    url(r'^remove/(?P<pk>\d+)/$', views.CourseDeleteView.as_view(), name='remove'),
     url(r'^(?P<pk>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
 )
