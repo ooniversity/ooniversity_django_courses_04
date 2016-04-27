d936c6b974b11e01c31a16d58fb403ec24cf1b0c
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-04-28 01:07:55 +0300

    Task_9_1_v2

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from students import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.StudentListView.as_view(), name='list_view'),
+    url(r'^$', views.StudentListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
     url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
     url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
