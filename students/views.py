eb6a98b60104c5a575f229e5a413971aa9897cee
nikolz_lin_Home <nikolzp@gmail.com>
2016-04-27 07:24:18 +0300

    9.1

--- students/views.py
+++ students/views.py
@@ -104,6 +104,11 @@ class StudentDeleteView(DeleteView):
 		context = super(StudentDeleteView, self).get_context_data(**kwargs)
 		context['title'] = 'Student info suppression'
 		return context
+		
+	def form_valid(self, form):
+		messages.success(self.request, "Info on %s %s has been successfully deleted." % (self.object.name, self.object.surname))
+		return super(StudentUpdateView, self).form_valid(form)
+
 
 
 
