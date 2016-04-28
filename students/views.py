f271fa44e0b1e3ed36ce29ca6ae4db7ee1415ae2
Ivan Babaiev <ivanbabaiev@gmail.com>
2016-04-28 12:54:11 +0300

    Added pagination Ver.1

--- students/views.py
+++ students/views.py
@@ -13,6 +13,7 @@ from students.models import Student
 
 class StudentListView(ListView):
     model = Student
+    paginate_by = 2
 
     def get_queryset(self):
         qs = super(StudentListView, self).get_queryset()
