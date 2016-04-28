26cf719cb0b11939384b7c83c7f81a1f5de54e24
navka666 <navka6677@gmail.com>
2016-04-28 11:13:35 +0300

    kz_10_1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from pybursa import views
+from feedbacks.views import FeedbackView
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
     url(r'^courses/', include('courses.urls', namespace="courses")),
     url(r'^students/', include('students.urls', namespace="students")),
     url(r'^coaches/', include('coaches.urls', namespace="coaches")),
+    url(r'^feedback/$', FeedbackView.as_view(), name='feedback'),
 )
