1c60db6b4d35a6a2f7f1afb8bd5722bb54f062a9
Vlad Durov <durov.vlad@gmail.com>
2016-04-29 16:01:32 +0300

    10.1 что ему нужно)

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 from django.contrib import admin
 
+from feedbacks.views import FeedbackView
 from pybursa import views
-
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^students/', include('students.urls', namespace='students')),
     url(r'^coaches/', include('coaches.urls', namespace='coaches')),
-    url(r'^feedback/', include('feedbacks.urls')),
+    url(r'^$', FeedbackView.as_view(), name='feedback'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     url(r'^contact/', views.contact, name='contact'),
