890b206972cf68fc1ad1f8319daa2b9037574ec7
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-04-29 23:02:57 +0300

    Task_9_1_v3

--- students/views.py
+++ students/views.py
@@ -1,53 +1,59 @@
-# -*- coding: utf-8 -*-
+from forms import StudentModelForm
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, redirect
-from students.forms import StudentModelForm
-from students.models import Student
+from django.template import RequestContext
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
+class StudentListView(ListView):
+	model = Student
 
-def list_view(request):
-    course_id = request.GET.get('course_id', None)
-    if course_id:
-        students = Student.objects.filter(courses__id=course_id)
-    else:
-        students = Student.objects.all()
-    return render(request, 'students/list.html', {'students': students})
-
-
-def detail(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    return render(request, 'students/detail.html', {'student': student})
-
-
-def create(request):
-    if request.method == "post":
-        form = StudentModelForm(request.POST)
-        if form.is_valid():
-            student = form.save()
-            messages.success(request, u'Студент {} {} успешно добавлен.'.format(student.name, student.surname))
-            return redirect('students:list')
-    else:
-        form = StudentModelForm()
-    return render(request, 'students/add.html', {'form': form})
-
-
-def edit(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    if request.method == "post":
-        form = StudentModelForm(request.POST, instance=student)
-        if form.is_valid():
-            student = form.save()
-            messages.success(request, u'Данные изменены.')
-            return redirect('students:edit', student.id)
-    else:
-        form = StudentModelForm(instance=student)
-    return render(request, 'students/edit.html', {'form': form})
-
-
-def remove(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    if request.method == "post":
-        student.delete()
-        messages.success(request, u'Студент {} {} был удален.'.format(student.name, student.surname))
-        return redirect('students:list')
-    return render(request, 'students/remove.html', {'student': student})
+  
+  class StudentDetailView(DetailView):
+  	model = Student
+ 	
+	def	get_queryset(self):
+		qs =  super(StudentDetailView, self).get_queryset()
+		course_id =self.request.GET.get('course_id', None)
+ 		if course_id:
+ 			qs = qs.filter(courses__id=course_id)
+ 		return qs
+ 
+class StudentCreateView(CreateView):
+	model = Student
+	success_url = reverse_lazy('students:list_view')
+ 	form_class = StudentModelForm
+ 	
+	def get_context_data(self,**kwargs):
+ 		context = super(StudentCreateView, self).get_context_data(**kwargs)
+ 		context['title'] = u"Student registration"
+ 		return context
+  
+	def form_valid(self,form):
+ 		student = form.save()
+ 		messages.success(self.request, u'Student %s %s has been successfully added.'%(student.name, student.surname))
+ 		return super(StudentCreateView, self).form_valid(form)	
+ 
+class StudentUpdateView(UpdateView):
+	model = Student
+ 	success_url = reverse_lazy('students:list_view')
+ 
+	def get_context_data(self,**kwargs):
+		context = super(StudentUpdateView, self).get_context_data(**kwargs)
+		context['title'] = u"Student info update"
+		return context
+ 	
+	def form_valid(self,form):
+ 		student = form.save()
+ 		messages.success(self.request, u'Student %s %s has been successfully added.'%(student.name, student.surname))
+		return super(StudentUpdateView, self).form_valid(form)
+ 
+class StudentDeleteView(DeleteView):
+	model = Student
+	success_url = reverse_lazy('students:list_view')
+ 
+    def get_context_data(self,**kwargs):
+		context = super(StudentDeleteView, self).get_context_data(**kwargs)
+		context['title'] = u"Student info suppression"
+		return context
