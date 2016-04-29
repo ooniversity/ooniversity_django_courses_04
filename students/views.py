2d6d55a7075e8e0e14c42af38bd4177a00905119
Dima Timoshenko <tzd0409@gmail.com>
2016-04-30 00:25:16 +0300

    kz 9_1 #5

--- students/views.py
+++ students/views.py
@@ -13,7 +13,6 @@ from students.forms import StudentModelForm
 class SrudentListView(ListView):
     model = Student
 
-
     def get_queryset(self):
         course_id = self.request.GET.get('course_id',None)
 
@@ -39,7 +38,8 @@ class StudentDetailView(DetailView):
 
 class StudentCreateView(CreateView):
     model = Student
-    success_url = reverse_lazy('students:list_view')
+    success_url = reverse_lazy('students:student_list')
+    
 
     def form_valid(self, form):
         message = u"Student %s %s has been successfully added."% (self.object.name, self.object.surname)
@@ -54,7 +54,7 @@ class StudentCreateView(CreateView):
 
 class StudentUpdateView(UpdateView):
     model = Student
-    success_url = reverse_lazy('students:list_view')
+    success_url = reverse_lazy('students:student_list')
     def get_context_data(self, **kwargs):
         context = super(StudentUpdateView, self).get_context_data(**kwargs)
         context['title'] = "Student info update"
@@ -62,13 +62,12 @@ class StudentUpdateView(UpdateView):
 
     def form_valid(self, form):
         message = super(StudentUpdateView, self).form_valid(form)
-        messages.success(self.request, u"Student %s %s has been successfully updated."
-                         % (self.object.name, self.object.surname))
+        messages.success(self.request, u"Student %s %s has been successfully updated."% (self.object.name, self.object.surname))
         return message    
 
 class StudentDeleteView(DeleteView):
     model = Student
-    success_url = reverse_lazy('students:list_view')
+    success_url = reverse_lazy('students:student_list')
 
     def form_valid(self, form):
         message = u"Student %s %s has been successfully deleted."% (self.object.name, self.object.surname)
