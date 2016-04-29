18864be4411a404a03fc8fcb6da6fbccce5fd3ed
Vlad Durov <durov.vlad@gmail.com>
2016-04-29 16:25:36 +0300

    10.1 fix import

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 
 class Feedback(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=150)
     subject = models.CharField(max_length=150)
     message = models.TextField()
     from_email = models.EmailField(verbose_name = 'Your Email')
     create_date = models.DateTimeField(auto_now_add = True)
-
-    def __unicode__(self):
-        return self.from_email
