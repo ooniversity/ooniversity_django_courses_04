b31a06ace07f860ec6c39a4e9f2e9ac709259bb8
chernish_s <sergey.chernish@gmail.com>
2016-04-30 15:12:37 +0300

    task9.1ver.1

--- students/urls.py
+++ students/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import patterns, url
 
 from students import views
 
-
 urlpatterns = patterns('',
-                       url(r'^(\d+)/$',
-                           views.student_detail_view, name="detail"),
-                       url(r'^$', views.student_list_view, name="list_view"),
-                       url(r'^add/$', views.create, name='add'),
-                       url(r'^remove/(\d+)/$', views.remove, name='remove'),
-                       url(r'^edit/(\d+)/$', views.edit, name='edit'),
+                       url(r'^$',
+                           views.StudentListView.as_view(), name='list_view'),
+                       url(r'^(?P<pk>\d+)/$',
+                           views.StudentDetailView.as_view(), name='detail'),
+                       url(r'^add/$',
+                           views.StudentCreateView.as_view(), name='add'),
+                       url(r'^remove/(?P<pk>\d+)/$',
+                           views.StudentDeleteView.as_view(), name='remove'),
+                       url(r'^edit/(?P<pk>\d+)/$',
+                           views.StudentUpdateView.as_view(), name='edit'),
                        )
