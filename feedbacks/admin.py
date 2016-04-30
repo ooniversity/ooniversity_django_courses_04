218766e0fc831ed0c4aa6a1e5d68c6b3d2e840b7
OlhaShyliaieva <olshilyaeva@gmail.com>
2016-04-30 03:12:11 +0300

    PyBursa Pagination

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -6,9 +6,5 @@ from feedbacks.models import Feedback
 class FeedbackAdmin(admin.ModelAdmin):
            
     list_display = ['from_email', 'create_date']
-    #def case_name(self, obj):
-    #    return ('%s %s' % (obj.name, obj.surname))
-    #case_name.short_description = "full name"
-
     
 admin.site.register(Feedback, FeedbackAdmin)
