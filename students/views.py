e2eb0e12ccb27ddf176f906a3466c7d7d31e8471
Mobil Home <mobil.light@gmail.com>
2016-04-30 17:32:16 +0300

    KZ_10_2 v.1.04

--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+#from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -13,33 +13,23 @@ from students.forms import StudentModelForm
 
 class StudentListView(ListView):
     model = Student
+    paginate_by = 2
 
-    def  get_queryset(self, course_id=None):
-        if course_id != None:
-            course_id = self.request.GET.get('course_id', None)
+    def  get_queryset(self):
+        course_id = self.request.GET.get('course_id', None)
+        if course_id:
             course = get_object_or_404(Course, id=course_id)
             list_of_students = course.student_set.all()
         else:
             list_of_students = Student.objects.all()
-        print(list_of_students)
         return list_of_students
 
     def get_context_data(self, **kwargs):
         context = super(StudentListView, self).get_context_data(**kwargs)
-        page = self.request.GET.get('page')
-        course_id = self.request.GET.get('course_id')
-        list_of_students = self.get_queryset(course_id)
-        paginator = Paginator(list_of_students, 2)
-        try:
-            contacts = paginator.page(page)
-        except PageNotAnInteger:
-            contacts = paginator.page(1)
-        except EmptyPage:
-            contacts = paginator.page(paginator.num_pages)
-        context['contacts'] = contacts
-        context['total_pages'] = paginator.page_range
-        context['course_id'] = course_id
+        context['course_id'] = self.request.GET.get('course_id', None)
         return context
+
+
 
 class StudentDetailView(DetailView):
     model = Student
