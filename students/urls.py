8b02c6e3354a742daa4ab30bdde0a88575d3f4bd
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-04-30 15:56:59 +0300

    task_9

--- students/urls.py
+++ students/urls.py
@@ -4,9 +4,9 @@ from students import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.StudentListView.as_view(), name='list'),
+    url(r'^$', views.StudentListView.as_view(), name='list_view'),
     url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
     url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
-    url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='remove'),
+    url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
 )
--- students/views.py
+++ students/views.py
@@ -1,66 +1,101 @@
-# -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from students.models import Student
-
+from courses.models import Course
+from students.forms import StudentModelForm
 
 class StudentListView(ListView):
     model = Student
 
-    def get_queryset(self):
-        qs = super(StudentListView, self).get_queryset()
-        course_id = self.request.GET.get('course_id', None)
-        if course_id:
-            qs = qs.filter(courses__id=course_id)
-        return qs
-
+    def  get_queryset(self):
+        if self.request.GET:
+            course_id = int(self.request.GET['course_id'])
+            course = get_object_or_404(Course, id=course_id)
+            list_of_students = course.student_set.all()
+        else:
+            list_of_students = Student.objects.all()
+        return list_of_students
 
 class StudentDetailView(DetailView):
     model = Student
 
-
 class StudentCreateView(CreateView):
     model = Student
-    success_url = reverse_lazy('students:list')
-
-    def form_valid(self, form):
-        super_valid = super(StudentCreateView, self).form_valid(form)
-        messages.success(self.request,
-                         u'Студент {} {} успешно добавлен.'.format(self.object.name, self.object.surname))
-        return super_valid
+    success_url = reverse_lazy('students:list_view')
 
     def get_context_data(self, **kwargs):
         context = super(StudentCreateView, self).get_context_data(**kwargs)
-        context.update({
-            "title": u'Создание нового студента'
-        })
+        context['title'] = "Student registration"
         return context
-
 
 class StudentUpdateView(UpdateView):
     model = Student
-    success_url = '/students/edit/%(id)d/'
-
-    def form_valid(self, form):
-        messages.success(self.request, u'Данные изменены.')
-        return super(StudentUpdateView, self).form_valid(form)
+    success_url = reverse_lazy('students:list_view')
 
     def get_context_data(self, **kwargs):
         context = super(StudentUpdateView, self).get_context_data(**kwargs)
-        context.update({
-            "title": u'Редактирование данных студента'
-        })
+        context['title'] = "Student info update"
         return context
-
 
 class StudentDeleteView(DeleteView):
     model = Student
-    success_url = reverse_lazy('students:list')
+    success_url = reverse_lazy('students:list_view')
 
-    def delete(self, request, *args, **kwargs):
-        delete_super = super(StudentDeleteView, self).delete(request, *args, **kwargs)
-        messages.success(self.request,
-                         u'Студент {} {} был удален.'.format(self.object.name, self.object.surname))
-        return delete_super
+    def get_context_data(self, **kwargs):
+        context = super(StudentDeleteView, self).get_context_data(**kwargs)
+        context['title'] = "Student info suppression"
+        return context
+		
+		
+#def detail(request, student_id):
+#    student = get_object_or_404(Student, id=int(student_id))
+#    student_courses_list = student.courses.all()
+#    return render(request, 'students/detail.html', {'student':student, 'students_courses':student_courses_list})
+ 
+#def list_view(request):
+#    if request.GET:
+#        course_id = int(request.GET['course_id'])
+#        course = get_object_or_404(Course, id=course_id)
+#        list_of_students = course.student_set.all()
+#    else:
+#       list_of_students = Student.objects.all()
+#
+#    return render(request, 'students/list.html', {'students_list': list_of_students})
+
+#def create(request):
+#    if request.method == "POST":
+#        form = StudentModelForm(request.POST)
+#        if form.is_valid():
+#            application = form.save()
+#            message =  u"Student %s %s has been successfully added." % (application.name, application.surname)
+#            messages.success(request, message)
+#            return redirect('students:list_view')
+#    else:
+#        form = StudentModelForm()
+#    return render(request, 'students/add.html', {'form':form})
+
+#def remove(request, student_id):
+#    student = get_object_or_404(Student, id=student_id)
+#    if request.method == "POST":
+#        message =  u"Info on %s %s has been sucessfully deleted." % (student.name, student.surname)
+#        student.delete()
+#        messages.success(request, message)
+#        return redirect('students:list_view')
+#    else:
+#        return render(request, 'students/remove.html', {'student':student})
+
+#def edit(request, student_id):
+#    student = get_object_or_404(Student, id=student_id)
+#    if request.method == "POST":
+#        form = StudentModelForm(request.POST, instance=student)
+#        if form.is_valid():
+#            form.save()
+#            messages.success(request, u"Info on the student has been sucessfully changed.")
+#            return render(request, 'students/edit.html', {'form':form})
+#    else:
+#        form = StudentModelForm(instance=student)
+#    return render(request, 'students/edit.html', {'form':form})
