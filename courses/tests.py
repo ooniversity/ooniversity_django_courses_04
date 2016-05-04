05ebeab63b7e73c4ed604f617de252e32e42001e
Mobil Work Ubunta <mobil.light@gmail.com>
2016-05-04 16:55:18 +0300

    KZ_11_2 c.0

--- courses/tests.py
+++ courses/tests.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.test import Client
 
 from courses.models import Course
 
 class CoursesListTest(TestCase):
+
     def test_list(self):
         client = Client()
         response = client.get('/')
@@ -18,4 +20,23 @@ class CoursesListTest(TestCase):
             short_description = 'Python Base Course')
         client = Client()
         response = client.get('/')
-        self.assertEqual(Course.objects.all(), 1)
+        self.assertEqual(Course.objects.all().count(), 2)
+
+    def test_404(self):
+        client = Client()
+        course1 = Course.objects.create(
+            name = 'Django Base',
+            short_description = 'Django Base Course')
+        response = client.get('/courses/2/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_titles(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'Курсы PyBursa')
+        self.assertContains(response, 'Program')
+
+    def test_template(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,55 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from students.models import Student
+
+class StudentsListTest(TestCase):
+
+    def test_list(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_content(self):
+        student1 = Student.objects.create(
+            name = 'Alex',
+            surname = 'First',
+            date_of_birth = '2000-02-10',
+            email = 'aa@aa.aa',
+            phone = 1111,
+            address = 'Adress',
+            skype = 'sk_ype',
+            courses = '1'
+            )
+        course2 = Course.objects.create(
+            name = 'Bob',
+            surname = 'Second',
+            date_of_birth = '1999-02-10',
+            email = 'bb@bb.bb',
+            phone = 2222,
+            address = 'Adress2',
+            skype = 'sk_ype2',
+            courses = '2'
+            )
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(Student.objects.all().count(), 2)
+
+    def test_404(self):
+        client = Client()
+        student1 = Student.objects.create(
+            name = 'Django Base',
+            short_description = 'Django Base Course')
+        response = client.get('/courses/2/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_titles(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, 'Список студентов')
+
+    def test_template(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
