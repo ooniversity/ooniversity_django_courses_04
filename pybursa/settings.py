3c36bde3224c43e9d48819d9316867396fdef6eb
Sergey Povazhnuk <sergant_ua@mail.ru>
2016-05-05 00:51:54 +0300

    KZ11_1 ver.01

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -99,3 +99,39 @@ EMAIL_HOST_PASSWORD = ''
 
 
 ADMINS = (("Sergey", "sergpo@mailru"),)
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'loggers': {
+        'courses': {
+            'handlers': ['file_courses'],
+            'level': 'DEBUG',
+        },
+        'students': {
+            'handlers': ['file_students'],
+            'level': 'WARNING',
+        },
+    },
+    'handlers': {
+        'file_courses': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'courses_logger.log'),
+            'formatter': 'simple'
+        },
+        'file_students': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'students_logger.log'),
+            'formatter': 'verbose'
+        },
+    },
+}
