b6a1c05f95d0b45dd81654d5d70f20949e4fb609
EqVert <EqVert@gmail.com>
2016-05-05 08:58:47 +0300

    KZ-11_P11

--- courses/tests.py
+++ courses/tests.py
@@ -4,6 +4,49 @@ from courses.models import Course
 from django.test import Client
 
 
+class CoursesListTest(TestCase):
+    def test_list(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class CoursesDetailTest(TestCase):
+    def test_detail_course(self):
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+
+
+
+
+'''
+    def test_content(self):
+        course1 = Course.objects.create(
+            name = 'PythonBase',
+            short_description = 'Основы программирования Python')
+        course2 = Course.objects.create(
+            name = 'PythonBase',
+            short_description = 'Основы программирования Python')
+        course3 = Course.objects.create(
+            name = 'PythonBase',
+            short_description = 'Основы программирования Python')
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(Course.objects.all(), 6)
+
+        course2 = Course.objects.create(
+            name = 'Python Base',
+            short_description = 'Python Base Course')
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(Course.objects.all(), 1)
+'''
+
+
+
+
+
+'''
 items = {
     'course1': {
         'name': 'PythonBase',
@@ -36,50 +79,4 @@ class CoursesListTest(TestCase):
 
 #        self.assertContains(response, "Python Base", status_code=200)
 
-
-
-
-
-
-'''
-from django.test import Client
-
-
-items = {
-    'course1': {
-        'name': 'Python Base',
-        'short_description': 'Основы программирования Python',
-    }}
-
-def create_item(item):
-    return Course.objects.create(**item)
-
-def create_items():
-    for item in items.keys():
-        create_item(items[item])
-
-class CoursesListTest(TestCase):
-    def test_course_create(self):
-        create_items()
-        self.assertEqual(Course.objects.all().count(), 2)
-        client = Client()
-        response = Client.get('/courses/')
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, "Course1", status_code=200)
-        self.assertContains(response, "Course2", status_code=200)
-
-#       course1 = Course.objects.create(
-#                                       name='Python Base',
-#                                       #short_description="Основы программирования Python"
-#                                       )
-#       self.assertEqual(Course.object.all().count(), 1)
-#   def test_pages(self):
-#       client = Client()
-#
-#       response = client.get('/courses/')
-#       self.assertEqual(response.status_code, 404)
-##
-#       response = client.get('/courses/1/')
-#       self.assertEqual(response.status_code, 200)
 '''
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from students.models import Student
 
-# Create your tests here.
+
+class StudentsListTest(TestCase):
+    def test_list_statuscode(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+class StudentsDetailTest(TestCase):
+    def test_detail_create(self):
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 404)
