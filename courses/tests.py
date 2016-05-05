0a5ac6145c8f05dd00b1b311012a484dc2a517d4
Sergey Povazhnuk <sergant_ua@mail.ru>
2016-05-06 01:35:27 +0300

    KZ11_2 ver.02

--- courses/tests.py
+++ courses/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
+from courses.models import Course, Lesson
 
 # Create your tests here.
-class CourseTests(TestCase):
-	def
+class CoursesListTest(TestCase):
+	def test_cource_create(self):
+		course1 = Course.objects.create(
+							name = 'PyBursa2',
+							short_description="Web development with django")
+		self.assertEqual(Course.objects.all().count(),1)
+
+	def 
+	
+
+#class CoursesDetailTest(TestCase):
+#	def
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class StudentsListTest(TestCase):
+	pass
+
+#class StudentsDetailTest(TestCase):
+#	def
