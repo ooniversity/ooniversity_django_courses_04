d1993f1bea3dda25fc0fc5aed11104a0b28acc45
EqVert <EqVert@gmail.com>
2016-05-05 10:54:13 +0300

    KZ-11_P12

--- courses/tests.py
+++ courses/tests.py
@@ -9,12 +9,70 @@ class CoursesListTest(TestCase):
         client = Client()
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
+    def test_list_curs(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'Курсы PyBursa')
+    def test_list_Description(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'Description')
+    def test_list_url(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+    def test_create_course(self):
+        client = Client()
+        Course.objects.create(
+            name='course_1',
+            short_description='В Этом Курсе Мы Сместили Акцент В Сторону Практики.',
+            )
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+'''
+
+    def test_course1_create(self):
+        client = Client()
+        course1 = Course.objects.create(
+            name = 'PythonBase',
+            short_description = 'Основы программирования Python')
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Course.objects.all().count(), 1)
+    def test_course2_create(self):
+        client = Client()
+        course1 = Course.objects.create(
+            name = 'PythonExtended',
+            short_description = 'Расширенный курс Python')
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+    def test_course3_create(self):
+        client = Client()
+        course1 = Course.objects.create(
+            name = 'Python Full',
+            short_description = 'Полный курс Python')
+        response = client.get('/courses/3/')
+        self.assertEqual(response.status_code, 200)
+    def test_course4_create(self):
+        client = Client()
+        course1 = Course.objects.create(
+            name = 'Python Gold',
+            short_description = 'Золотая серия курсов. Для избранных.')
+        response = client.get('/courses/4/')
+        self.assertEqual(response.status_code, 200)
+'''
 
 class CoursesDetailTest(TestCase):
     def test_detail_course(self):
         client = Client()
         response = client.get('/courses/1/')
         self.assertEqual(response.status_code, 404)
+
+        course = Course.objects.create(
+            name = 'Python Base',
+            short_description = 'Основы программирования Python')
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
 
 
 
