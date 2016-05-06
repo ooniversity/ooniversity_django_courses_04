2747f99cef92a7f020d921dbbf54e9185ae688c0
Nikolz <nikolzp@gmail.com>
2016-05-06 17:00:32 +0300

    11.2

--- students/tests.py
+++ students/tests.py
@@ -25,5 +25,39 @@ class StudentsListTest(TestCase):
 		self.assertTemplateNotUsed(response, 404)
 
 	def test_students(self):
-		course1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
 		self.assertEqual(Student.objects.all().count(), 1)
+
+class StudentsDetailTest(TestCase):
+	def test_page(self):
+		client = Client()
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		response = client.get('/students/1/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_template(self):
+		client = Client()
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		response = client.get('/students/1/')
+		self.assertTemplateUsed(response, 'students/student_detail.html')
+
+	def test_title(self):
+		client = Client()
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		response = client.get('/students/1/')
+		self.assertContains(response, 'Student Detail')
+
+	def test_not_template(self):
+		client = Client()
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		response = client.get('/students/1/')
+		self.assertTemplateNotUsed(response, 404)
+
+	def test_redirect(self):
+		client = Client()
+		steudent1 = Student.objects.create(name='Юрий', surname='Махно', date_of_birth='2016-10-15')
+		response = client.get('/students/1')
+		self.assertEqual(response.status_code, 301)
+
+
+
