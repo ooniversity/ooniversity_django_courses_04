9885b747b2cf35d05348e38af26c9ad0916ebf3b
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-05-06 13:05:10 +0300

    Tests_01

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,128 @@
+-*- coding: utf-8 -*-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from coaches.models import Coach
+from django.test import Client
+from courses.models import Course, Lesson
 
-# Create your tests here.
+ 
+class CoursesListTest(TestCase):
+    def test_list_get_active_url(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+		
+	def test_list_page(self):
+	    client = Client()
+		response = client.get('/courses/1/')
+		self.assertEqual(response.status_code, 404)
+
+    def test_list_new_course(self):
+        client = Client()
+        create_coach()
+        create_course()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+  
+    def test_list_course_title(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, u'Сведенья о курсе - pyBursa')   
+
+    def test_list_using_template(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+		
+    def test_list_set_of_tests(self):		
+		client = Client()
+ 		response = client.get('/')
+ 		self.assertEqual(response.status_code, 200)
+ 		course1 = Course.objects.create(
+			name = 'test 1',
+			short_description = 'This is first courses test',
+			description = '1 course')
+		course2 = Course.objects.create(
+			name = 'test 2',
+			short_description = 'This is second courses test',
+			description = '2 course')
+        course3 = Course.objects.create(
+			name = 'test 3',
+			short_description = 'This is third courses test',
+			description = '3 course')
+		response = client.get('/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(Course.objects.all().count(), 3)
+ 
+	def test_list_addlink(self):
+		response = self.client.get('/')
+ 		self.assertContains(response, '/courses/add/')
+
+	def test_list_removelink(self):
+		course = Course.objects.create(
+ 			name = 'test 3',
+ 			short_description = 'This is third courses test',
+ 			description = '3 course')
+ 		response = self.client.get('/')
+ 		self.assertContains(response, '/courses/remove/{}/'.format(course.id))
+ 
+    def test_list_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+ 
+
+ 
+ 
+class CoursesDetailTest(TestCase):
+ 
+    def test_detail_newcoach(self):
+        client = Client()
+        create_coach()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_detail_newcourse(self):
+        client = Client()
+        create_coach()
+        create_course()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_detail_coursecontent(self):
+        create_full_course()
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertContains(response, 'course_1')   
+ 
+    def test_detail_item(self):
+ 		client = Client()
+ 		response = client.get('/courses/1/')
+ 		self.assertEqual(response.status_code, 404)
+ 		course = Course.objects.create(
+ 			name = 'test 1',
+ 			short_description = 'This is first courses test',
+ 			description = '1 course')
+ 		response = client.get('/courses/1/')
+ 		self.assertEqual(response.status_code, 200)
+ 
+ 	def test_detail_coursedescription(self):
+		client = Client()
+ 		response = client.get('/courses/2/')
+ 		self.assertEqual(response.status_code, 404)
+ 		course = Course.objects.create(
+ 			name = 'test 2',
+ 			short_description = 'This is second courses test',
+ 			description = '2 course') 
+ 		response = client.get('/courses/2/')
+ 		self.assertEqual(response.status_code, 200)
+ 		self.assertContains(response, course.description)
+  
+    def test_detail_response_statuscode(self):
+        for page in ['1', '2', '3']:
+            response = self.client.get('/courses/%s/' % page)
+            self.assertEqual(response.status_code, 200)
+ 
+    def test_detail_incorrect_course(self):
+        response = self.client.get('/courses/10/')
+        self.assertEqual(response.status_code, 404)
+ 
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,73 @@
+-*- coding: utf-8 -*-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from coaches.models import Coach
+from django.test import Client
+from courses.models import Course, Lesson
 
-# Create your tests here.
+
+class StudentsListTest(TestCase): 
+    def test_list_statuscode(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_list_content(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, 'Список студентов')
+ 
+    def test_list_activeurl(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertTemplateUsed(response, 'students/student_list.html')
+    
+    def test_list_template(self):
+        response = self.client.get('/students/')
+        self.assertTemplateUsed(response, 'students/student_list.html')
+
+    def test_list_response(self):
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_list_activeurl(self):
+        response = self.client.get('/students/')
+        self.assertTemplateUsed(response, 'students/student_list.html')
+		
+	def test_list_addlink(self):
+		response = self.client.get('/')
+ 		self.assertContains(response, '/students/add/')
+
+class StudentsDetailTest(TestCase):
+
+    def test_detail_create(self):
+        create_student('new')
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertContains(response, 'new')		
+ 
+    def test_detail_student_statuscode(self):
+        response = self.client.get('/students/%d'%get_random_student().id)
+        self.assertEqual(response.status_code, 301)
+
+    def test_detail_response(self):
+        for page in ['1', '2', '3']:
+            response = self.client.get('/students/%s/' % page)
+            self.assertEqual(response.status_code, 200)
+ 
+    def test_detail_incorrect_info(self):
+        response = self.client.get('/students/12/')
+        self.assertEqual(response.status_code, 404)
+ 
+    def test_detail_activeurl(self):
+        response = self.client.get('/students/1/')
+        self.assertTemplateUsed(response, 'students/student_detail.html') 
+		
+	def test_detail_response_statuscode(self):
+        for page in ['1', '2']:
+            response = self.client.get('/students/%s/' % page)
+            self.assertEqual(response.status_code, 200)
+ 
+    def test_detail_incorrect_student(self):
+        response = self.client.get('/students/12/')
+        self.assertEqual(response.status_code, 404)
