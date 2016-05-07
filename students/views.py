33c6a2b27b9657a253f86fc1465ed54beef4e377
chernish_s <sergey.chernish@gmail.com>
2016-05-07 15:54:49 +0300

    Vanga resolvbe issues

--- students/views.py
+++ students/views.py
@@ -31,6 +31,16 @@ class StudentListView(ListView):
 class StudentDetailView(DetailView):
     model = Student
 
+    def get_context_data(self, **kwargs):
+        logger.debug("Students detail view has been debugged")
+        logger.info("Logger of students detail view informs you!")
+        logger.warning("Logger of students detail view warns you!")
+        logger.error("Students detail view went wrong!")
+        student = self.get_object()
+        context = super(StudentDetailView, self).get_context_data(**kwargs)
+        context['title'] = u"Student %s %s detail" % (student.name, student.surname)
+        return context
+
 
 class StudentCreateView(CreateView):
     model = Student
