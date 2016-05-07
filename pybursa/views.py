5f709953c0b4f189d378738588a0fbaab78b9c68
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-05-07 23:15:07 +0300

    Task12_1_v1

--- pybursa/views.py
+++ pybursa/views.py
@@ -12,12 +12,12 @@ def contact(request):
 
 	
 def custom_404_server_error(request):
-    response = render_to_response('page_404.html')
+    response = render_to_response('404.html')
     response.status_code = 404
     return response
  
  
 def custom_500_server_error(request):
-    response = render_to_response('page_500.html')
+    response = render_to_response('500.html')
     response.status_code = 500
     return response
