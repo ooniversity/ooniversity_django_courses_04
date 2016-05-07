bd4ebb1590760cea96b97b9ed8b3471a10d3fcc5
VaschenkoMargarita <vaschenko.margarita44@gmail.com>
2016-05-07 23:02:45 +0300

    Pages_404&500

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from courses.models import Course
 
 
@@ -9,3 +9,15 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+	
+def custom_404_server_error(request):
+    response = render_to_response('page_404.html')
+    response.status_code = 404
+    return response
+ 
+ 
+def custom_500_server_error(request):
+    response = render_to_response('page_500.html')
+    response.status_code = 500
+    return response
