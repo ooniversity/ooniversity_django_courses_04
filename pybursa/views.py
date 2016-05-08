3fae54d89eec6be303c46fbc0f2525f58f85c5fc
morozov_n <nickmetal@yandex.ua>
2016-05-08 21:50:32 +0300

    add cw12_1 404 500 templates

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 from django.shortcuts import render
-from courses.models import Course
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from courses.models import Course
 
 
 def index(request):
@@ -34,3 +36,16 @@ def contact(request):
 def student_detail(request):
     return render(request, 'student_detail.html')
 
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
