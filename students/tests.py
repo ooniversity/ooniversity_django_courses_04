db2c97455cfd3611ce28a85eff21125cc8f265f0
chernish_s <sergey.chernish@gmail.com>
2016-05-09 12:38:06 +0300

    testing ver 2

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from students.models import Student
+
+
+def create_student(st_name):
+    Student.objects.create(
+        name="Petr",
+        surname="Petrov",
+        date_of_birth="1990-05-05",
+        email="PPetrov@mail.ru",
+        phone="12345678",
+        address="my long address",
+        skype="PPetrov",
+    )
+
+
+class StudentsListTest(TestCase):
+
+    def test_list_statuscode(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_header(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, 'Students')
+
+    def test_list_url_active(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertTemplateUsed(response, 'students/student_list.html')
+
+    def test_pagination(self):
+        for i in ['First', 'Second', 'Third']:
+            create_student(i)
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, 'next >>')
+
+
+class StudentsDetailTest(TestCase):
+
+    def test_detail_statuscode(self):
+        for i in ['First', 'Second', 'Third']:
+            create_student(i)
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_redirect(self):
+        for i in ['First', 'Second', 'Third']:
+            create_student(i)
+        client = Client()
+        response = client.get('/students/2')
+        self.assertEqual(response.status_code, 301)
+
+    def test_detail_header(self):
+        for i in ['First', 'Second', 'Third']:
+            create_student(i)
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertContains(response, 'Petr Petrov')
+
+    def test_detail_url_active(self):
+        for i in ['First', 'Second', 'Third']:
+            create_student(i)
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertTemplateUsed(response, 'students/student_detail.html')
