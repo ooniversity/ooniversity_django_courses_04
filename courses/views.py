44a00c360f14a43566a005acd8bd9b13b8ecc328
SouthBut <yukhnokostya@gmail.com>
2016-05-11 16:54:59 +0300

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import logging
 from django.shortcuts import render, redirect
 from courses.models import Course, Lesson
 from courses.forms import CourseModelForm, LessonModelForm
@@ -8,13 +9,17 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse, reverse_lazy
 
-
+logger = logging.getLogger(__name__)
 class CourseDetailView(DetailView):
 	model = Course
 	template_name = 'courses/detail.html'
 	context_object_name = 'course'
 
 	def get_context_data(self, **kwargs):
+		logger.debug("Courses detail view has been debugged")
+		logger.info("Logger of courses detail view informs you!")
+		logger.warning("Logger of courses detail view warns you!")
+		logger.error("Courses detail view went wrong!")
 		context = super(CourseDetailView,self).get_context_data(**kwargs)
 		context["title"] = "Course detail"
 		pk = self.kwargs['pk']
