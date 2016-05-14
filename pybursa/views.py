41f13af36cd68fffbf90fb204383980b09d3de8d
Dima Timoshenko <tzd0409@gmail.com>
2016-05-15 00:35:53 +0300

    KZ 12_1

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from courses.models import Course, Lesson	
 
 
@@ -13,4 +13,15 @@ def student_list(request):
     return render(request, '../templates/student_list.html', )
 
 def student_detail(request):
-    return render(request, '../templates/student_detail.html', )            
+    return render(request, '../templates/student_detail.html', )       
+
+def custom_404_server_error(request):
+    response = render_to_response('404.html')
+    response.status_code = 404
+    return response
+
+
+def custom_500_server_error(request):
+    response = render_to_response('500.html')
+    response.status_code = 500
+    return response     
