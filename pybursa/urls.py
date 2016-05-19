a152cb240f74c7f92f21963b40b06b4d70265370
s_shishka@ukr.net <you@example.com>
2016-05-19 16:38:21 +0300

    Completed control task 3

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from pybursa import views
+
 urlpatterns = patterns(
     '',
     # Examples:
@@ -9,4 +11,8 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^student_list/$', views.student_list, name='student_list'),
+    url(r'^student_detail/$', views.student_detail, name='student_detail'),
     )
