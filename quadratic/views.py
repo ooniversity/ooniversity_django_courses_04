27a3b25a66ba48d6e301685ee4d3826290fadbda
s_shishka@ukr.net <you@example.com>
2016-05-25 11:12:32 +0300

    ct-4 linked equation page to the navbar, redesign views logic

--- quadratic/views.py
+++ quadratic/views.py
@@ -5,46 +5,57 @@ from django.shortcuts import render
 def quadratic_results(request):
     req_data = request.GET
 
-    a = req_data['a']
-    str_a = ""
-    if a == "":
-        str_a = "коэффициент не определен"
-    else:
-        try:
-            a = int(a)
-        except ValueError:
-                a = str(a)
-                str_a = "коэффициент не целое число"
-        if a == 0:
-            a = """коэффициент при первом слагаемом уравнения,
-                не может быть равным нулю"""
+    if req_data:
+        a = req_data['a']
+        str_a = ""
+        if a == "":
+            str_a = "коэффициент не определен"
+        else:
+            try:
+                a = int(a)
+            except ValueError:
+                    a = str(a)
+                    str_a = "коэффициент не целое число"
+            if a == 0:
+                str_a = """коэффициент при первом слагаемом уравнения,
+                    не может быть равным нулю"""
 
-    b = (req_data['b'])
-    str_b = ""
-    if b == "":
-        str_b = "коэффициент не определен"
-    else:
-        try:
-            b = int(b)
-        except ValueError:
-            b = str(b)
-            str_b = "коэффициент не целое число"
+        b = (req_data['b'])
+        str_b = ""
+        if b == "":
+            str_b = "коэффициент не определен"
+        else:
+            try:
+                b = int(b)
+            except ValueError:
+                b = str(b)
+                str_b = "коэффициент не целое число"
 
-    c = (req_data['c'])
-    str_c = ""
-    if c == "":
-        str_c = "коэффициент не определен"
-    else:
-        try:
-            c = int(c)
-        except ValueError:
-            c = str(c)
-            str_c = "коэффициент не целое число"
+        c = (req_data['c'])
+        str_c = ""
+        if c == "":
+            str_c = "коэффициент не определен"
+        else:
+            try:
+                c = int(c)
+            except ValueError:
+                c = str(c)
+                str_c = "коэффициент не целое число"
 
-    return render(request, 'results.html', {'a': a,
-                                            'b': b,
-                                            'c': c,
-                                            'str_a': str_a,
-                                            'str_b': str_b,
-                                            'str_c': str_c
-                                            })
+        context_data = {
+                    'a': a,
+                    'b': b,
+                    'c': c,
+                    'str_a': str_a,
+                    'str_b': str_b,
+                    'str_c': str_c
+                    }
+
+        return render(request, 'quadratic/results.html', context_data)
+
+    else:
+        context_data = {'string': "Квадратное уравнение a*x*x + b*x + c = 0",
+                        'info': '''Для произведения вычислений,
+                        добавьте значения коэффициентов в строку URL
+                        в формате: ?a=1&b=3&c=5'''}
+        return render(request, 'quadratic/results.html', context_data)
