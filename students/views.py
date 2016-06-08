487b9d71f451358d2c706acfd68e3687564da967
s_shishka@ukr.net <you@example.com>
2016-06-08 15:31:37 +0300

    ct-5 completed first part

--- students/views.py
+++ students/views.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import Student, Course
 
-# Create your views here.
+
+def list_view(request):
+    course_id = request.GET.get('course_id')
+    if course_id:
+        students = Student.objects.filter(courses=course_id)
+        course = Course.objects.get(id=course_id)
+        return render(request, 'students/list.html', {'students': students,
+                                                      'course': course})
+    else:
+        students = Student.objects.all()
+        return render(request, 'students/list.html',
+                      {'students': students})
+
+
+def detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    return render(request, 'students/detail.html', {'student': student})
