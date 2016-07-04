9da5c656e0ccfe170d131fe7466ec344105bb67c
s_shishka@ukr.net <you@example.com>
2016-07-04 21:43:13 +0300

    ct-7 completed

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.list_view, name='list_view'),
     url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<student_id>\d+)/$', views.remove, name='remove'),
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'),
 )
--- students/views.py
+++ students/views.py
@@ -27,7 +27,7 @@ def create(request):
     if request.method == 'POST':
         form = StudentModelForm(request.POST)
         if form.is_valid():
-            student = form.save()
+            student_form = form.save()
             messages.success(
                 request, "Student %s %s has been successfully added!" %
                 (student.name, student.surname))
@@ -35,3 +35,29 @@ def create(request):
     else:
         form = StudentModelForm()
     return render(request, 'students/add.html', {'form': form})
+
+
+def edit(request, student_id):
+    student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST, instance=student)
+        if form.is_valid():
+            edit_form = form.save()
+            messages.success(
+                request, "Info on the student has been sucessfully changed!")
+            return redirect('students:edit', student.id)
+    else:
+        form = StudentModelForm(instance=student)
+    return render(request, 'students/edit.html', {'form': form})
+
+
+def remove(request, student_id):
+    student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        student.delete()
+        messages.success(
+            request, "Info on %s %s has been successfully deleted!" %
+            (student.name, student.surname))
+        return redirect('students:list_view')
+    else:
+        return render(request, 'students/remove.html', {'student': student})
