3398ce5249ff85ba7d220abd8b4cbaeea578c52d
Sergey <s_shishka@ukr.net>
2016-07-06 17:20:57 +0300

    ct-8 completed

--- courses/views.py
+++ courses/views.py
@@ -19,13 +19,13 @@ def add(request):
         if form.is_valid():
             course = form.save()
             messages.success(
-                request, "Course %s has been successfully added!" % 
+                request, "Course %s has been successfully added!" %
                 course.name)
         return redirect('index')
     else:
         form = CourseModelForm()
     return render(request, 'courses/add.html', {'form': form})
- 
+
 
 def edit(request, course_id):
     course = Course.objects.get(id=course_id)
@@ -37,7 +37,7 @@ def edit(request, course_id):
         return redirect('courses:edit', course.id)
     else:
         form = CourseModelForm(instance=course)
-    return render(request, 'courses/edit.html', {'form': form})   
+    return render(request, 'courses/edit.html', {'form': form})
 
 
 def remove(request, course_id):
@@ -49,3 +49,17 @@ def remove(request, course_id):
         return redirect('index')
     else:
         return render(request, 'courses/remove.html', {'course': course})
+
+
+def add_lesson(request, course_id):
+    if request.method == "POST":
+        form = LessonModelForm(request.POST)
+        if form.is_valid():
+            lesson = form.save()
+            messages.success(
+                request, "Lesson %s has been successfully added!" %
+                lesson.subject)
+        return redirect('courses:detail', course_id)
+    else:
+        form = LessonModelForm(initial={'course': course_id})
+    return render(request, 'courses/add_lesson.html', {'form': form})
